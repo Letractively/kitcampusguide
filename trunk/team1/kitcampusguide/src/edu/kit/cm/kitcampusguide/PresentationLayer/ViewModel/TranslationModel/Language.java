765d684da6b8e33c198bac267324d3d43c004986
frederik.diehl@student.kit.edu <frederik.diehl@student.kit.edu@02b7392b-9286-25d4-34bb-9ed0ff935e76>
2010-12-26 18:01:46 +0000

    Language changed to construct itself form a specified xml-file.
    Added JavaDocs in both Language and TranslationModel.

--- trunk/team1/kitcampusguide/src/edu/kit/cm/kitcampusguide/PresentationLayer/ViewModel/TranslationModel/Language.java
+++ trunk/team1/kitcampusguide/src/edu/kit/cm/kitcampusguide/PresentationLayer/ViewModel/TranslationModel/Language.java
@@ -1,21 +1,60 @@
 package edu.kit.cm.kitcampusguide.PresentationLayer.ViewModel.TranslationModel;
-
-import java.util.Map;
+import java.io.IOException;
+import java.util.HashMap;
+import org.jdom.*;
+import org.jdom.input.SAXBuilder;
 
 class Language {
-	private Map<String, String> translateMap;
+	/** Stores the keys and values for the translation.*/ 
+	private HashMap<String, String> translateMap;
 	
+	/** Stores the name of the language.*/
 	private String name;
 	
+	/**
+	 * Translates the <code>key</code> in the language represented by this class.
+	 * Returns <code>null</code> if there ist no translation for this key.
+	 * @param key The key to be translated.
+	 * @return The translation or <code>null</code>.
+	 */
 	String tr(String key) {
 		return translateMap.get(key);
 	}
 	
+	/**
+	 * Returns the name of the language.
+	 * @return The name of the language.
+	 */
 	String getName() {
 		return name;
 	}
 	
+	/**
+	 * Constructs a new language object from the xml-file specified by filename.
+	 * Needs to have a special format which will be specified later.
+	 * @param filename The path to the file the language is defined in.
+	 */
 	Language(String filename) {
-		//TODO Implement extraction from file.
+		translateMap = new HashMap<String, String>();
+		SAXBuilder builder = new SAXBuilder();
+		try {
+			Document doc = builder.build(filename);
+			Element root = doc.getRootElement();
+			constructLanguage(root);
+		} catch (JDOMException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	private void constructLanguage(Element root) {
+		Element child = (Element) root.getChildren().get(0);
+		name = child.getValue();
+		for (int i = 1; i < root.getChildren().size(); i++) {
+			child = (Element) root.getChildren().get(i);
+			translateMap.put(child.getName(), child.getValue());	
+		}
 	}
 }
--- trunk/team1/kitcampusguide/src/edu/kit/cm/kitcampusguide/PresentationLayer/ViewModel/TranslationModel/TranslationModel.java
+++ trunk/team1/kitcampusguide/src/edu/kit/cm/kitcampusguide/PresentationLayer/ViewModel/TranslationModel/TranslationModel.java
@@ -3,28 +3,58 @@ package edu.kit.cm.kitcampusguide.PresentationLayer.ViewModel.TranslationModel;
 import java.util.List;
 
 public class TranslationModel {
+	/** Stores the current language.*/
 	private Language currentLanguage;
+	
+	/** Stores the {@link LanguageManager} this instance uses.*/
 	private LanguageManager manager;
 	
+	/**
+	 * Constructs a new {@link TranslationModel}.
+	 */
 	public TranslationModel() {
 		manager = LanguageManager.getInstance();
 		currentLanguage = manager.getDefaultLanguage();
 	}
 	
+	/**
+	 * Translates the {@link String} <code>key</code> in the current language.
+	 * If there is no value for <code>key</code> in the current language the corresponding value from
+	 * the default language will be used. Returns <code>null</code> only if there is no value for <code>key</code> in both the current and the default language.
+	 * @param key The key identifying the text to be translated.
+	 * @return The text corresponding to <code>key</code> in the current language or the default language or <code>null</code> if it doesn't exist in either.
+	 */
 	public String tr(String key) {
-		return currentLanguage.tr(key);
+		String result = currentLanguage.tr(key);
+		if (result == null) {
+			result = manager.getDefaultLanguage().tr(key);
+		}
+		return result;
 	}
 	
+	/**
+	 * Sets the current language to the language identified by <code>language</code>.
+	 * Best used with {@link getLanguages()}. Only changes the language if it is available on the system.
+	 * @param language The <code>String</code> identifying the language. 
+	 */
 	public void setCurrentLanguage(String language) {
 		if (manager.getLanguageByString(language) != null) {
 			currentLanguage = manager.getLanguageByString(language);
 		}
 	}
 	
+	/**
+	 * Returns the name of the current language.
+	 * @return The name of the current language.
+	 */
 	public String getCurrentLanguage() {
 		return currentLanguage.getName();
 	}
 	
+	/**
+	 * Returns a list of the names of every available language.
+	 * @return A list of the names of every available language.
+	 */
 	public List<String> getLanguages() {
 		return manager.getLanguagesAsString();
 	}
