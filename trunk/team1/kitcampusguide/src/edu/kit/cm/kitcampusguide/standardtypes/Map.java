4c760ca9c7a6e4bc8a7f337b747467845e07ffca
frederik.diehl@student.kit.edu <frederik.diehl@student.kit.edu@02b7392b-9286-25d4-34bb-9ed0ff935e76>
2011-01-05 11:13:24 +0000

    Map: Added the function getTilesURL to get the position where the tiles are saved. Added the function getBoundingBox to get the MapSection defining the boundaries of the map. Changed the constructor accordingly to include a MapSection and a String.
    Saves the tilesURL and the boundingBox as private variables.

--- trunk/team1/kitcampusguide/src/edu/kit/cm/kitcampusguide/standardtypes/Map.java
+++ trunk/team1/kitcampusguide/src/edu/kit/cm/kitcampusguide/standardtypes/Map.java
@@ -16,6 +16,12 @@ public class Map {
 	/** Saves the name of the map*/
 	private String name;
 	
+	/** Saves the bounding box of the map*/
+	private MapSection boundingBox;
+	
+	/** Saves the URL leading to the tiles*/
+	private String tilesURL;
+	
 	/** Saves a collection of all maps.*/
 	private static Collection<Map> maps = new ArrayList<Map>();
 	
@@ -23,10 +29,14 @@ public class Map {
 	 * Creates a new map.
 	 * @param id ID of the new map. Is required to be unique for a correct result. However, this is not tested.
 	 * @param name Name of the map. Required not to be <code>null</code>.
+	 * @param boundingBox The boundaries of this map. Required not to be <code>null</code>.
+	 * @param tilesURL The path to the files.
 	 */
-	public Map(int id, String name) {
+	public Map(int id, String name, MapSection boundingBox, String tilesURL) {
 		this.id = id;
 		this.name = name;
+		this.boundingBox = boundingBox;
+		this.tilesURL = tilesURL;
 		maps.add(this);
 	}
 	
@@ -47,6 +57,22 @@ public class Map {
 	}
 	
 	/**
+	 * Returns the bounding box of the map.
+	 * @return The bounding box of the map.
+	 */
+	public MapSection getBoundingBox() {
+		return boundingBox;
+	}
+	
+	/**
+	 * Returns the URL of the tiles.
+	 * @return The URL of the tiles.
+	 */
+	public String getTilesURL() {
+		return tilesURL;
+	}
+	
+	/**
 	 * Returns the map with the ID <code>id</code>. 
 	 * If there are two maps with this id, only one is returned. However, no guarantee of this is given for future versions.
 	 * @param id The map with this ID will be returned.
@@ -62,5 +88,4 @@ public class Map {
 		}
 		return result;
 	}
-	
 }
