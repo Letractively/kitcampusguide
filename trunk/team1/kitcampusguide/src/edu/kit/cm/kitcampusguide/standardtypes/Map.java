6ffb26485b4f7af7686928973ac1a0e1320af55a
frederik.diehl@student.kit.edu <frederik.diehl@student.kit.edu@02b7392b-9286-25d4-34bb-9ed0ff935e76>
2011-01-12 17:40:16 +0000

    Map: Added the attributes minZoom and maxZoom, changed constructor accordingly. Answer to issue 14.

--- trunk/team1/kitcampusguide/src/edu/kit/cm/kitcampusguide/standardtypes/Map.java
+++ trunk/team1/kitcampusguide/src/edu/kit/cm/kitcampusguide/standardtypes/Map.java
@@ -22,8 +22,16 @@ public class Map {
 	/** Saves the URL leading to the tiles*/
 	private String tilesURL;
 	
+	/** Saves the minimal zoom level of the map.*/
+	private int minZoom;
+	
+	/** Saves the maximal zoom level of the map.*/
+	private int maxZoom;
+	
 	/** Saves a collection of all maps.*/
 	private static Collection<Map> maps = new ArrayList<Map>();
+	
+	
 	
 	/**
 	 * Creates a new map.
@@ -31,12 +39,27 @@ public class Map {
 	 * @param name Name of the map. Required not to be <code>null</code>.
 	 * @param boundingBox The boundaries of this map. Required not to be <code>null</code>.
 	 * @param tilesURL The path to the files.
+	 * @param minZoom The minimal zoom level for this map. Required to be >= 0. Required to be <= <code>maxZoom</code>.
+	 * @param maxZoom The maximal zoom level for this map. Required to be >= 0. Required to be >= <code>minZoom</code>.
 	 */
-	public Map(int id, String name, MapSection boundingBox, String tilesURL) {
+	public Map(int id, String name, MapSection boundingBox, String tilesURL, int minZoom, int maxZoom) {
+		if (name == null || boundingBox == null || tilesURL == null) {
+			throw new NullPointerException();
+		}
+		if (minZoom > maxZoom) {
+			throw new IllegalArgumentException("Map with ID " + id + ": minZoom > maxZoom.");
+		}
+		for (Map map: maps) {
+			if (map.getID() == id) {
+				throw new IllegalArgumentException("Map with ID " + id + " already exists.");
+			}
+		}
 		this.id = id;
 		this.name = name;
 		this.boundingBox = boundingBox;
 		this.tilesURL = tilesURL;
+		this.minZoom = minZoom;
+		this.maxZoom = maxZoom;
 		maps.add(this);
 	}
 	
@@ -70,6 +93,22 @@ public class Map {
 	 */
 	public String getTilesURL() {
 		return tilesURL;
+	}
+	
+	/**
+	 * Returns the minimal zoom level.
+	 * @return The minimal zoom level.
+	 */
+	public int getMinZoom() {
+		return minZoom;
+	}
+	
+	/**
+	 * Returns the maximal zoom level.
+	 * @return The maximal zoom level.
+	 */
+	public int getMaxZoom() {
+		return maxZoom;
 	}
 	
 	/**
