77c0ec92a70946ed2a4322198fe6bf4a2734b643
kateryna.yurchenko <kateryna.yurchenko@student.kit.edu>
2011-01-17 10:34:58 +0000

    Klasse HeadlineModel hinzugefügt.

---
+++ team2/src/edu/kit/cm/kitcampusguide/model/HeadlineModel.java
@@ -0,0 +1,55 @@
+package edu.kit.cm.kitcampusguide.model;
+
+import java.util.ArrayList;
+
+/**
+ * This class contains the information about the headline which contains
+ * a search input field.
+ * 
+ * @author Kateryna Yurchenko
+ * 
+ */
+
+public class HeadlineModel {
+
+	/* The name of the place to be searched.*/
+	private String search;
+	
+	/* The suggestions for the input. */
+	private ArrayList<POI> suggestions;
+	
+	/**
+	 * Creates a new headline which contains a search input field.
+	 */
+	public HeadlineModel() {
+		this.search = null;
+		this.suggestions = null;
+	}
+	
+	/**
+	 * This method returns the name of the place to be searched.
+	 * 
+	 * @return the name of the place to be searched.
+	 */
+	public String getSearch() {
+		return this.search;
+	}
+		
+	/**
+	 * This method sets the name of the place to be searched.
+	 * 
+	 * @param the name of the place to be searched.
+	 */
+	public void setSearch(String search) {
+		this.search = search; 
+	}
+	
+	/**
+	 * This method sets the suggestions for the input.
+	 * 
+	 * @param the suggestions for the input.
+	 */
+	public void setSuggestions(ArrayList<POI> suggestions) {
+		this.suggestions = suggestions; 
+	}	
+}
