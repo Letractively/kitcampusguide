284a1f0d9adefb4571e21da51a9159b2f3320774
kateryna.yurchenko <kateryna.yurchenko@student.kit.edu>
2011-01-17 15:55:42 +0000

    Klasse InfoboxModel hinzugefügt

---
+++ team2/src/edu/kit/cm/kitcampusguide/model/InfoboxModel.java
@@ -0,0 +1,62 @@
+package edu.kit.cm.kitcampusguide.model;
+
+/**
+ * This class contains the description of the infobox which contains the information about a POI.
+ * 
+ * @author Kateryna Yurchenko
+ * 
+ */
+
+public class InfoboxModel {
+
+	/* Is true if the further information is shown, otherwise false. */
+	private boolean extendedView;
+	
+	/* The POI which is corresponding to the infobox. */
+	private POI dedicatedPOI;
+	
+	/**
+	 * Creates a new infobox which contains the information about a POI.
+	 */
+	public InfoboxModel() {
+		this.extendedView = false;
+		this.dedicatedPOI = null;
+	}
+	
+	/**
+	 * This method returns true if the further information about a POI is shown,
+	 * otherwise false.
+	 * 
+	 * @return true if the further information is shown, otherwise false.
+	 */
+	public boolean getExtendedView() {
+		return this.extendedView;
+	}
+		
+	/**
+	 * This method changes the visibility of the further information about a POI.
+	 * 
+	 * @param extendedView the new visibility of the further information about a POI.
+	 */
+	public void setExtendedView(boolean extendedView) {
+		this.extendedView = extendedView; 
+	}
+	
+	/**
+	 * This method returns a POI which is corresponding to the infobox.
+	 * 
+	 * @return a POI which is corresponding to the infobox.
+	 */
+	public POI getDedicatedPOI() {
+		return this.dedicatedPOI;
+	}
+		
+	/**
+	 * This method sets a POI which is corresponding to the infobox.
+	 * 
+	 * @param dedicatedPOI a POI which must be set as a POI which is corresponding to the infobox.
+	 */
+	public void setDedicatedPOI(POI dedicatedPOI) {
+		this.dedicatedPOI = dedicatedPOI; 
+	}	
+}
