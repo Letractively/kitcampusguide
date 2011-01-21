f50957fc0f2ffaad11d50b763081c483c8b32a4b
isabel.funke <isabel.funke@student.kit.edu>
2011-01-21 19:49:56 +0000

    implemented drop-down-boxes for search requests with multiple results. (unfortunately it's not perfect yet ...) for testing, the inputlistener works with a test-poisource at the moment (i also committed it) so, just type in "testVorschlagsliste" to see my efforts ;)

---
+++ team1/kitcampusguide/src/edu/kit/cm/kitcampusguide/applicationlogic/poisource/TestPOISource.java
@@ -0,0 +1,73 @@
+package edu.kit.cm.kitcampusguide.applicationlogic.poisource;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import javax.faces.model.SelectItem;
+
+import edu.kit.cm.kitcampusguide.standardtypes.Building;
+import edu.kit.cm.kitcampusguide.standardtypes.Category;
+import edu.kit.cm.kitcampusguide.standardtypes.Map;
+import edu.kit.cm.kitcampusguide.standardtypes.MapSection;
+import edu.kit.cm.kitcampusguide.standardtypes.POI;
+import edu.kit.cm.kitcampusguide.standardtypes.WorldPosition;
+
+public class TestPOISource implements POISource {
+	
+	private static Map map1 = new Map(42, "", new MapSection(new WorldPosition(0.0,0.0), new WorldPosition(0.0,0.0)), "", 0,0);
+	private static List<Category> list = Collections.emptyList();
+	private static POI poi1 = new POI("hallo1", "vorschlag1", "", new WorldPosition(0.0,0.0), 
+			map1, new Integer(1), list);
+	private static POI poi2 = new POI("hallo2", "vorschlag2", "", new WorldPosition(0.0,0.0), 
+			map1, new Integer(1), list);
+	private static POI poi3 = new POI("hallo3", "vorschlag3", "", new WorldPosition(0.0,0.0), 
+			map1, new Integer(1), list);
+	
+	
+	@Override
+	public Collection<POI> getPOIsBySection(MapSection section, Map map,
+			Collection<Category> categories) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<POI> getPOIsBySearch(String searchTerm) {
+		List<POI> resultList = new ArrayList<POI>();	
+		if (searchTerm.equals("testVorschlagsliste")) {		
+			resultList.add(poi1);
+			resultList.add(poi2);
+			resultList.add(poi3);
+			return resultList;
+		} else if (searchTerm.equals("testVorschlagsliste2")) {
+			resultList.add(poi1);
+			resultList.add(poi3);
+			return resultList;
+		} else if (searchTerm.equals("vorschlag1")) {
+			resultList.add(poi1);
+			return resultList;
+		} else if (searchTerm.equals("vorschlag2")) {
+			resultList.add(poi2);
+			return resultList;
+		} else if (searchTerm.equals("vorschlag3")) {
+			resultList.add(poi3);
+			return resultList;
+		} else return null;
+	}
+
+	@Override
+	public POI getPOIByID(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<POI> getPOIsByBuilding(Building building,
+			Collection<Category> categories) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- team1/kitcampusguide/src/edu/kit/cm/kitcampusguide/presentationlayer/viewmodel/InputModel.java
+++ team1/kitcampusguide/src/edu/kit/cm/kitcampusguide/presentationlayer/viewmodel/InputModel.java
@@ -1,15 +1,22 @@
 package edu.kit.cm.kitcampusguide.presentationlayer.viewmodel;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 
 import edu.kit.cm.kitcampusguide.standardtypes.*;
 
 import javax.faces.context.FacesContext;
+import javax.faces.component.UIComponent;
 import javax.faces.component.UIInput;
+import javax.faces.component.UISelectItem;
+import javax.faces.component.html.HtmlSelectOneMenu;
+import javax.faces.model.SelectItem;
 
 /**
  * Manages the inputs in all text fields in the view.
@@ -26,7 +33,9 @@ public class InputModel {
 	@ManagedProperty (value ="")
 	private String routeToField;
 	/** The proposal list to be displayed after a search. Can be <code>null</code>.*/
-	private List<POI> proposalList;
+	private List<SelectItem> routeFromProposalList = new ArrayList<SelectItem>();
+	private List<SelectItem> routeToProposalList = new ArrayList<SelectItem>();
+
 	/** The export link to be displayed. Can be <code>null</code>.*/
 	private String exportLink;
 	/** The html embedding code. Can be <code>null</code>.*/
@@ -72,21 +81,37 @@ public class InputModel {
 		}
 	}
 
+	public List<SelectItem> getRouteFromProposalList() {
+		return routeFromProposalList;
+	}
+	
+	public void setRouteFromProposalList(List<SelectItem> routeFromProposalList) {
+		this.routeFromProposalList = routeFromProposalList;
+	}	
+	
+	public List<SelectItem> getRouteToProposalList() {
+		return routeToProposalList;
+	}
+	
+	public void setRouteToProposalList(List<SelectItem> routeToProposalList) {
+		this.routeToProposalList = routeToProposalList;
+	}	
+
 	/**
 	 * Returns the proposal list.
 	 * @return The proposal list. Can be <code>null</code>.
 	 */
-	public List<POI> getProposalList() {
+	/* List<POI> getProposalList() {
 		return proposalList;
-	}
+	}*/
 
 	/**
 	 * Sets the content of the proposal list.
 	 * @param proposalList The content of the proposal list. Can be <code>null</code>.
 	 */
-	public void setProposalList(List<POI> proposalList) {
+	/*public void setProposalList(List<POI> proposalList) {
 		this.proposalList = proposalList;					
-	}
+	}*/
 
 	/**
 	 * Returns the export link string.
