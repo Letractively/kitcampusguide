6f3e3c8e3b580900420e0aef60fdf8d0c1d939c4
tobias.zuendorf <tobias.zuendorf@student.kit.edu>
2011-01-22 16:57:36 +0000

    Neue Methode zu MapLoader hinzugefügt

--- team2/src/edu/kit/cm/kitcampusguide/data/MapLoader.java
+++ team2/src/edu/kit/cm/kitcampusguide/data/MapLoader.java
@@ -2,6 +2,12 @@ package edu.kit.cm.kitcampusguide.data;
 
 import edu.kit.cm.kitcampusguide.model.*;
 
+/**
+ * 
+ * @author Michael Hauber
+ * @author Tobias Zündorf
+ *
+ */
 public interface MapLoader {
 	
 	/**
@@ -9,5 +15,15 @@ public interface MapLoader {
 	 * @return
 	 */
 	public Graph getGraph();
+	
+	/**
+	 * Creates a Graph containing intersections, which are stored in the database, but not the streets 
+	 * connecting them. Additional the Graph contains some special nodes, named landmarks, with degree zero. 
+	 * For each pair of intersection-node and landmark-node the Graph contains an edge with their distance.
+	 * 
+	 * @return A Graph representing the map with additional landmarks
+	 */
+	// TODO zum Entwurf hinzufügen
+	public Graph getLandmarks();
 
 }
