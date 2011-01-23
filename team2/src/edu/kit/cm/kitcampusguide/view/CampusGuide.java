d303d4c03744952610abf9efa3994948a5bba7d0
haoqian.zheng <haoqian.zheng@student.kit.edu>
2011-01-23 16:00:25 +0000

    More view Stuff

--- team2/src/edu/kit/cm/kitcampusguide/view/CampusGuide.java
+++ team2/src/edu/kit/cm/kitcampusguide/view/CampusGuide.java
@@ -1,17 +1,52 @@
 package edu.kit.cm.kitcampusguide.view;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 
 import edu.kit.cm.kitcampusguide.model.HeadlineModel;
+import edu.kit.cm.kitcampusguide.model.InfoboxModel;
+import edu.kit.cm.kitcampusguide.model.Settings;
+import edu.kit.cm.kitcampusguide.model.SidebarModel;
 
 @ManagedBean
+@SessionScoped
 public class CampusGuide {
 	
 	HeadlineModel hlm;
+	SidebarModel sbm;
+	InfoboxModel ibm;
+	Settings settings;
 	
 	public CampusGuide() {
 		this.hlm = new HeadlineModel();
 		this.hlm.setSearch("test");
+		this.sbm = new SidebarModel();
+		this.ibm = new InfoboxModel();
+		this.settings = null;
+	}
+
+	public SidebarModel getSbm() {
+		return sbm;
+	}
+
+	public void setSbm(SidebarModel sbm) {
+		this.sbm = sbm;
+	}
+
+	public InfoboxModel getIbm() {
+		return ibm;
+	}
+
+	public void setIbm(InfoboxModel ibm) {
+		this.ibm = ibm;
+	}
+
+	public Settings getSettings() {
+		return settings;
+	}
+
+	public void setSettings(Settings settings) {
+		this.settings = settings;
 	}
 
 	public HeadlineModel getHlm() {
