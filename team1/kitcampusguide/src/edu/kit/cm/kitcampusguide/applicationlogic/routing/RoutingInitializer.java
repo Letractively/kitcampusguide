dee2911ad272a40dc1dac5e22d171ab201e1e05e
frederik.diehl <frederik.diehl@student.kit.edu>
2011-01-28 10:38:01 +0000

    RoutingInitializer: Fixed a bug concerning  multiple tags at the construction of routing graph. Added comments. DijkstraRoutingCalculation: Added a debug message.

--- team1/kitcampusguide/src/edu/kit/cm/kitcampusguide/applicationlogic/routing/RoutingInitializer.java
+++ team1/kitcampusguide/src/edu/kit/cm/kitcampusguide/applicationlogic/routing/RoutingInitializer.java
@@ -89,32 +89,51 @@ public class RoutingInitializer {
 	 * @param map The Map used for a vertice if no other is specified.
 	 */
 	private void constructGraph(Document document, Map map) {
+		//initializes the required local variables.
 		Element root = document.getRootElement();
 		List<Element> wayList = root.getChildren("way");
 		List<MapPosition> positionList = new ArrayList<MapPosition>();
 		HashMap<Integer, Element> allNodes = new HashMap<Integer, Element>();
+		
+		//Fills the HashMap allNodes with all node elements.
 		List<Element> nodes = root.getChildren("node");
 		for (Element e : nodes) {
 			allNodes.put(new Integer(e.getAttributeValue("id")), e);
 		}
+		
+		//Constructs the nodes via searching ways with the "highway" tag.
+		//The Map nodeList maps the ref node-id of the osm representation to the counter which 
+		//will later be equivalent to the node id inside the routing graph.
 		HashMap<Integer, Integer> nodeList = new HashMap<Integer, Integer>();
 		int counter = 0;
 		for (Element currentWay : wayList) {
+			//Tests if the currently watched way has the highway tag.
 			boolean isHighway = false;
 			try {
-				isHighway = currentWay.getChild("tag").getAttribute("k").getValue().equalsIgnoreCase("highway");
+				List<Element> tags = currentWay.getChildren("tag");
+				for (Element tag : tags) {
+					if (tag.getAttribute("k").getValue().equalsIgnoreCase("highway")) {
+						isHighway = true;
+						break;
+					}
+				}
 			} catch(NullPointerException e) {
 				
 			}
+			//if it has the highway attribute, all elements (= nodes) will be adressed separately
 			if (isHighway) {
 				List<Element> currentWayNodes = currentWay.getChildren("nd");
 				for (Element node : currentWayNodes) {
+					//Checks the id of the node, if it is already contained in nodeList.
 					int nodeToBeChecked = Integer.parseInt(node.getAttributeValue("ref"));
 					if (!nodeList.containsKey(nodeToBeChecked)) {
+						//If it isn't contained, it is put in nodeList, and the position is constructed. 
 						nodeList.put(Integer.parseInt(node.getAttributeValue("ref")), new Integer(counter));
 						Element tmp = allNodes.get(Integer.parseInt(node.getAttributeValue("ref")));
 						double lon = Double.parseDouble(tmp.getAttributeValue("lon"));
 						double lat = Double.parseDouble(tmp.getAttributeValue("lat"));
+						//If the node has no "mapID" attribute (custom added, not part of osm), the standard map
+						//given by the parameter is used.
 						Map poiMap = map;
 						if (tmp.getAttribute("mapID") != null) {
 							poiMap = Map.getMapByID(Integer.parseInt(tmp.getAttributeValue("mapID")));
@@ -126,18 +145,28 @@ public class RoutingInitializer {
 			}
 			
 		}
+		
+		//The edges are constructed.
 		List<Integer>[] edgesLists = new ArrayList[nodeList.size()];
 		for (int i = 0; i < nodeList.size(); i++) {
 			edgesLists[i] = new ArrayList<Integer>();
 		}
 		for (Element currentWay : wayList) {
+			//by looking if the way has the tag highway
 			boolean isHighway = false;
 			try {
-				isHighway = currentWay.getChild("tag").getAttribute("k").getValue().equalsIgnoreCase("highway");
+				List<Element> tags = currentWay.getChildren("tag");
+				for (Element tag : tags) {
+					if (tag.getAttribute("k").getValue().equalsIgnoreCase("highway")) {
+						isHighway = true;
+						break;
+					}
+				}
 			} catch(NullPointerException e) {
 				
 			}
 			if (isHighway) {
+				//And if so, adding the respectively other node to the nodes from and which the way originates.
 				List<Element> nodeListOfCurrentWay = currentWay.getChildren("nd");
 				for (int i = 0; i < nodeListOfCurrentWay.size() - 1; i++) {
 					Integer from = new Integer(nodeListOfCurrentWay.get(i).getAttributeValue("ref")); 
@@ -147,13 +176,18 @@ public class RoutingInitializer {
 				}
 			}
 		}
+		//The list of vertices and list of edges are constructed.
 		List<Integer> verticesList = new ArrayList<Integer>();
 		List<Integer> edgeList = new ArrayList<Integer>();
 		for (int i = 0; i < edgesLists.length; i++) {
+			//first by concatenating the edgeList and constructing the vertice list.
 			verticesList.add(new Integer(edgeList.size()));
 			edgeList.addAll(edgesLists[i]);
 		}
+		//and the dummy element.
 		verticesList.add(edgeList.size());
+		
+		//And lastly converting all lists to arrays.
 		verticesArray = new int[verticesList.size()];
 		for (int i = 0; i < verticesList.size(); i++) {
 			verticesArray[i] = verticesList.get(i).intValue();
@@ -167,6 +201,7 @@ public class RoutingInitializer {
 			positionArray[i] = positionList.get(i);
 		}
 		
+		//And constructing the weights, by calculating the distance between the two vertices.
 		weightArray = new double[edgeArray.length];
 		for (int i = 0; i < verticesArray.length - 1; i++) {
 			int[] neighbours = getNeighbours(i);
@@ -174,6 +209,7 @@ public class RoutingInitializer {
 				weightArray[verticesArray[i] + j] = calculateDistance(positionArray[i], positionArray[j]);
 			}
 		}
+		//And initializing the RoutingGraph object.
 		RoutingGraph.initializeGraph(verticesArray, edgeArray, weightArray, positionArray);
 	}
 	
