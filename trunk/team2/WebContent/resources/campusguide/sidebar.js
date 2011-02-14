c73d55721d167b55836d411aacbbd3514f2593cc
haoqian.zheng@student.kit.edu <haoqian.zheng@student.kit.edu@02b7392b-9286-25d4-34bb-9ed0ff935e76>
2011-02-14 23:52:46 +0000

    hide route button added

--- trunk/team2/WebContent/resources/campusguide/sidebar.js
+++ trunk/team2/WebContent/resources/campusguide/sidebar.js
@@ -1,14 +1,30 @@
+hideRoute();
+
 function route() {
-	routeLayer.removeAllFeatures();
-	current_route = getElement("route:current-route");
+	hideRoute();
+	current_route = getElementInnerHTML("route:current-route");
 	if (current_route != null) {
 		var newRoute = new Array();
 		for (var i = 0; i < current_route.length; i++) {
 			var ll = createLonLat(current_route[i].lon, current_route[i].lat);
 			newRoute.push(new OpenLayers.Geometry.Point(ll.lon, ll.lat));
 		}
+		var routeStyle = {
+				fillColor: "#0000FF",
+				strokeColor: "#0000FF",
+				strokeOpacity: 1,
+				strokeWidth: 3,
+				pointRadius: 6,
+				pointerEvents: "visiblePainted"
+			};
 		var routeFeature = new OpenLayers.Feature.Vector(
-			new OpenLayers.Geometry.LineString(newRoute, null, null));
-		routeLayer.addFeatures([ routeFeature ]);	
+			new OpenLayers.Geometry.LineString(newRoute, null, null), null, routeStyle);
+		routeLayer.addFeatures([ routeFeature ]);
+		getElement("route:hide-route").style.visibility = 'visible';
 	} 
+}
+
+function hideRoute() {
+	routeLayer.removeAllFeatures();
+	getElement("route:hide-route").style.visibility = 'hidden';
 }
