30c57afeb458f16daa08a2d2a6b85a99fb5c528f
michael.hauber2 <michael.hauber2@student.kit.edu>
2011-02-15 02:50:25 +0000

    creating Testclasses for data

--- team2/test/data/ConcreteMapLoaderTest.java
+++ team2/test/data/ConcreteMapLoaderTest.java
@@ -1,6 +1,45 @@
 package data;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class ConcreteMapLoaderTest {
-
+	
+	
+	/*
+	@BeforeClass
+	public void beforeClass() {
+		
+	}
+	
+	@Before
+	public void before() {
+		
+	}
+	*/
+	@Test
+	public void Test() {
+		// tba
+	}
+	
+	/*
+	@Test
+	public void edgeTest() {
+		
+	}
+	
+	@After
+	public void after() {
+		
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		
+	}
+	*/
 }
--- team2/test/data/ConcretePOILoaderTest.java
+++ team2/test/data/ConcretePOILoaderTest.java
@@ -1,6 +1,47 @@
 package data;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class ConcretePOILoaderTest {
 
+
+	/*
+	@BeforeClass
+	public void beforeClass() {
+		
+	}
+	
+	@Before
+	public void before() {
+		
+	}
+	*/
+	@Test
+	public void Test() {
+		// tba
+	}
+	
+	/*
+	@Test
+	public void edgeTest() {
+		
+	}
+	
+	@After
+	public void after() {
+		
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		
+	}
+	*/
+	
+	
 }
