3e279ee9a31b9bd80fd0fd05172c7e8e16220358
Stefan.Altmayer@student.kit.edu <Stefan.Altmayer@student.kit.edu@02b7392b-9286-25d4-34bb-9ed0ff935e76>
2011-03-02 12:04:05 +0000

    Javadocs

--- trunk/team1/kitcampusguide/src/edu/kit/cm/kitcampusguide/presentationlayer/view/MapLocator.java
+++ trunk/team1/kitcampusguide/src/edu/kit/cm/kitcampusguide/presentationlayer/view/MapLocator.java
@@ -5,13 +5,35 @@ import java.io.Serializable;
 import edu.kit.cm.kitcampusguide.standardtypes.MapSection;
 import edu.kit.cm.kitcampusguide.standardtypes.WorldPosition;
 
-
-// TODO: Javadocs
+/**
+ * This class is either used to determine a position or a section on a map.
+ * 
+ * @see WorldPosition
+ * @see MapSection
+ * @author Stefan
+ * 
+ */
 public class MapLocator implements Serializable {
 
+	/**
+	 * The map section described by this instance. Either this or
+	 * <code>center</code> is <code>null</code>.
+	 */
 	private MapSection mapSection;
+	
+	/**
+	 * The world position described by this instance. Either this or
+	 * <code>mapSection</code> is <code>null</code>.
+	 */
 	private WorldPosition center;
 
+	/**
+	 * Creates a new <code>MapLocator</code> describing a given
+	 * {@link MapSection}.
+	 * 
+	 * @param mapSection
+	 *            the map section which will be described.
+	 */
 	public MapLocator(MapSection mapSection) {
 		if (mapSection == null) {
 			throw new NullPointerException();
@@ -19,6 +41,13 @@ public class MapLocator implements Serializable {
 		this.mapSection = mapSection;
 	}
 
+	/**
+	 * Creates a new <code>MapLocator</code> describing a given
+	 * {@link WorldPosition}.
+	 * 
+	 * @param center
+	 *            the world position which will be described.
+	 */
 	public MapLocator(WorldPosition center) {
 		if (center == null) {
 			throw new NullPointerException();
@@ -26,10 +55,22 @@ public class MapLocator implements Serializable {
 		this.center = center;
 	}
 	
+	/**
+	 * Returns the map section described by this instance.
+	 * 
+	 * @return a map section or <code>null</code> if this instance describes a
+	 *         world position.
+	 */
 	public MapSection getMapSection() {
 		return mapSection;
 	}
 	
+	/**
+	 * Returns the world position described by this instance.
+	 * 
+	 * @return a world position or <code>null</code> if this instance describes
+	 *         a map section.
+	 */
 	public WorldPosition getCenter() {
 		return center;
 	}
