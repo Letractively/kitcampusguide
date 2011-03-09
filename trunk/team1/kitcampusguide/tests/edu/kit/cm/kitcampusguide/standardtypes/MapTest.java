ac304d431d396cb1875d28eb848a57681f2f2b12
fabian.hentschel@student.kit.edu <fabian.hentschel@student.kit.edu@02b7392b-9286-25d4-34bb-9ed0ff935e76>
2011-03-09 09:48:51 +0000

    Added: Idgenerator.java Provides a framework to get unique IDs for the Category-, Map-, Building- and POI-Objects. Changed: MapTest.java Now uses the framework provided by Idgenerator.java. Added JavaDocs.

--- trunk/team1/kitcampusguide/tests/edu/kit/cm/kitcampusguide/standardtypes/MapTest.java
+++ trunk/team1/kitcampusguide/tests/edu/kit/cm/kitcampusguide/standardtypes/MapTest.java
@@ -5,11 +5,17 @@ import static org.junit.Assert.*;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.LinkedList;
-import java.util.List;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+import static edu.kit.cm.kitcampusguide.testframework.Idgenerator.*;
 
+/**
+ * Tests the implementation of Map.
+ * Note: Not thread safe.
+ * @author Fabian
+ *
+ */
 public class MapTest {
 
 	private static Map map;
@@ -27,11 +33,19 @@ public class MapTest {
 		map = new Map(mapID, "map1", boundingbox, "null", 0, 0);
 	}
 	
+	/**
+	 * Tests if the constructor correctly throws a <code>NullPointerException</code> 
+	 * in the event that <code>boundingBox</code> is <code>null</code>.
+	 */
 	@Test(expected=NullPointerException.class)
 	public void testBoundingBoxNull() {
 		new Map(getFreeMapID(), "map1", null, "null", 0, 0);
 	}
 	
+	/**
+	 * Tests if the constructor correctly throws an <code>IllegalArgumentException</code>
+	 * in the event that <code>minZoom</code> is greater than <code>maxZoom</code>.
+	 */
 	@Test(expected=IllegalArgumentException.class)
 	public void testMinZoomGreaterMaxZoom() {
 		WorldPosition pos1 = new WorldPosition(0, 0);
@@ -40,6 +54,10 @@ public class MapTest {
 		new Map(getFreeMapID(), "map1", boundingbox, "null", 1, 0);
 	}
 	
+	/**
+	 * Tests if the constructor correctly throws a <code>NullPointerException</code> 
+	 * in the event that <code>name</code> is <code>null</code>.
+	 */
 	@Test(expected=NullPointerException.class)
 	public void testNameNull() {
 		WorldPosition pos1 = new WorldPosition(0, 0);
@@ -48,6 +66,10 @@ public class MapTest {
 		new Map(getFreeMapID(), null, boundingbox, "null", 0, 0);
 	}
 	
+	/**
+	 * Tests if the constructor correctly throws a <code>NullPointerException</code> 
+	 * in the event that <code>tilesURL</code> is <code>null</code>.
+	 */
 	@Test(expected=NullPointerException.class)
 	public void testTilesUrlNull() {
 		WorldPosition pos1 = new WorldPosition(0, 0);
@@ -56,6 +78,10 @@ public class MapTest {
 		new Map(getFreeMapID(), "map1", boundingbox, null, 0, 0);
 	}
 	
+	/**
+	 * Tests if the constructor correctly throws an <code>IllegalArgumentException</code>
+	 * in the event that the ID provide by <code>id</code> is already taken.
+	 */
 	@Test(expected=IllegalArgumentException.class)
 	public void testMapIDTaken() {
 		WorldPosition pos1 = new WorldPosition(0, 0);
@@ -64,17 +90,25 @@ public class MapTest {
 		new Map(mapID, "map1", boundingbox, "null", 0, 0);
 	}
 	
-	
+	/**
+	 * Tests {@link Map#getID()}
+	 */
 	@Test
 	public void testGetID() {
 		assertEquals(mapID, map.getID());
 	}
 
+	/**
+	 * Tests {@link Map#getName()}
+	 */
 	@Test
 	public void testGetName() {
 		assertEquals("map1", map.getName());
 	}
 
+	/**
+	 * Tests {@link Map#getBoundingBox()}
+	 */
 	@Test
 	public void testGetBoundingBox() {
 		WorldPosition pos1 = new WorldPosition(0, 0);
@@ -84,26 +118,41 @@ public class MapTest {
 	}
 	
 
+	/**
+	 * Tests {@link Map#getTilesURL()}
+	 */
 	@Test
 	public void testGetTilesURL() {
 		assertEquals("null", map.getTilesURL());
 	}
 
+	/**
+	 * Tests {@link Map#getMinZoom()}
+	 */
 	@Test
 	public void testGetMinZoom() {
 		assertEquals(0, map.getMinZoom());
 	}
 
+	/**
+	 * Tests {@link Map#getMaxZoom()}
+	 */
 	@Test
 	public void testGetMaxZoom() {
 		assertEquals(0, map.getMaxZoom());
 	}
 
+	/**
+	 * Tests {@link Map#getMapByID(int)}
+	 */
 	@Test
 	public void testGetMapByID() {
 		assertSame(map, Map.getMapByID(mapID));
 	}
 
+	/**
+	 * Tests {@link Map#getBuilding()} with and without building associated to the map.
+	 */
 	@Test
 	public void testGetBuilding() {
 		assertNull(map.getBuilding());
@@ -112,30 +161,15 @@ public class MapTest {
 		WorldPosition pos2 = pos1;
 		MapSection boundingbox = new MapSection(pos1, pos2);
 		Map map2 = new Map(getFreeMapID(), "map1", boundingbox, "null", 0, 0);
-		Building b;
-		int id = 0;
-		do {
-			b = Building.getBuildingByID(id);
-			id++;
-		} while (b != null);
-		
+		int bID = getFreeBuildingID();
 		LinkedList<Map> floors = new LinkedList<Map>(
 				Arrays.asList(	new Map(getFreeMapID(), "map1", boundingbox, "null", 0, 0),
 								new Map(getFreeMapID(), "map1", boundingbox, "null", 0, 0),
 								map2));
 		Collection<Category> categories = new LinkedList<Category>();
-		POI buildingPOI = new POI("poiid", "poi", "poi", pos1, map, id, categories);
-		b = new Building(id, floors , 1, buildingPOI);
+		POI buildingPOI = new POI(getFreePOIID(), "poi", "poi", pos1, map, bID, categories);
+		Building b = new Building(bID, floors , 1, buildingPOI);
 		
 		assertSame(b, map2.getBuilding());
-	}
-
-	public static int getFreeMapID() {
-		Map m;
-		int i = 0;
-		do {
-			m = Map.getMapByID(++i);
-		} while (m != null);
-		return i;
 	}
 }
