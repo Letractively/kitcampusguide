385e7b7fecb5aafcc2952102e27f741b0181fa00
michael.hauber2 <michael.hauber2@student.kit.edu>
2011-03-10 21:13:45 +0000

    update tests

--- team2/test/data/ConcretePOILoaderTest.java
+++ team2/test/data/ConcretePOILoaderTest.java
@@ -1,11 +1,16 @@
 package data;
 
+import java.util.List;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import edu.kit.cm.kitcampusguide.model.POI;
+import edu.kit.cm.kitcampusguide.model.POICategory;
 
 public class ConcretePOILoaderTest {
 
@@ -21,10 +26,61 @@ public class ConcretePOILoaderTest {
 		
 	}
 	*/
+	
+	
+	
+	/**
+	 * getPOI(Integer id)
+	 */
 	@Test
-	public void Test() {
+	public void test1() {
 		// tba
 	}
+	
+	/**
+	 * getPOIsByName(String name)
+	 */
+	@Test
+	public void test2() {
+		// tba
+	}
+	
+	/**
+	 * getAllPOIs()
+	 */
+	@Test
+	public void test3() {
+		// tba
+	}
+	
+	/**
+	 * getPOICategory(Integer id)
+	 */
+	@Test
+	public void test4() {
+		// tba
+	}
+	
+	
+	/**
+	 * getPOICategoryByName(String name)
+	 */
+	@Test
+	public void test5() {
+		// tba
+	}
+	
+	/**
+	 * getAllPOICategory
+	 */
+	@Test
+	public void test6() {
+		// tba
+	}
+	
+	
+	
+	
 	
 	/*
 	@Test
