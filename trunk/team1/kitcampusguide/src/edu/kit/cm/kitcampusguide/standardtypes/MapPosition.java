cb3bc923711df84964e004396b591b6787c170d1
Stefan.Altmayer@student.kit.edu <Stefan.Altmayer@student.kit.edu@02b7392b-9286-25d4-34bb-9ed0ff935e76>
2011-03-13 14:45:42 +0000

    Added Javadoc files

--- trunk/team1/kitcampusguide/src/edu/kit/cm/kitcampusguide/standardtypes/MapPosition.java
+++ trunk/team1/kitcampusguide/src/edu/kit/cm/kitcampusguide/standardtypes/MapPosition.java
@@ -2,7 +2,7 @@ package edu.kit.cm.kitcampusguide.standardtypes;
 
 
 /**
- * Represents a position on a {@link Map map}. Extends {@link WorldPosition}.
+ * Represents a position on a {@link Map}. Extends {@link WorldPosition}.
  * Stores a {@Map map} additionally to the longitude and latitude stored by {@link WorldPosition}.
  * Positions representing the same longitude and latitude on the same map don't need to be the same object, rather,
  * due to longitude and latitude being double, it's better to create a new MapPosition each time it is needed.
@@ -10,14 +10,14 @@ package edu.kit.cm.kitcampusguide.standardtypes;
  *
  */
 public class MapPosition  extends WorldPosition {	
-	/** Saves the {@link Map map} the position lies on.*/
+	/** Saves the {@link Map} the position lies on.*/
 	private final Map map;
 	
 	/**
 	 * Constructs a new <code>MapPosition</code>.
 	 * @param latitude The latitude of the position.
 	 * @param longitude The longitude of the position.
-	 * @param map The {@link Map map} the position lies on.
+	 * @param map The {@link Map} the position lies on.
 	 * 
 	 * @throws IllegalArgumentException If either latitude or longitude are out of bounds.
 	 */
@@ -27,8 +27,8 @@ public class MapPosition  extends WorldPosition {
 	}
 
 	/**
-	 * Returns the {@link Map map} the position lies on.
-	 * @return The {@link Map map} the position lies on.
+	 * Returns the {@link Map} the position lies on.
+	 * @return The {@link Map} the position lies on.
 	 */
 	public Map getMap() {
 		return map;
