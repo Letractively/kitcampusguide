da7442302a51d81deefabe7254353edf0edd8351
kateryna.yurchenko <kateryna.yurchenko@student.kit.edu>
2011-03-13 16:50:43 +0000

    ConstantData und JavaScript testen

---
+++ team2/test/beans/ConstantDataTest.java
@@ -0,0 +1,33 @@
+package beans;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import edu.kit.cm.kitcampusguide.ConstantData;
+
+/**
+ * This class tests the ManagedBean {@see ConstantData} of the KITCampusGuide. 
+ * 
+ * @author Kateryna Yurchenko
+ */
+public class ConstantDataTest {
+
+	@Test
+	public void testConstantData() {
+		ConstantData c = new ConstantData();
+		Assert.assertNotNull(c);
+		Assert.assertNotNull(c.getCategories());
+		Assert.assertNotNull(c.getCategoriesName());
+		Assert.assertNotNull(c.getAllPOI());
+		Assert.assertNotNull(c.getAllPOIName());
+		Assert.assertFalse(c.getCategories().size() == 0);
+		Assert.assertFalse(c.getCategoriesName().size() == 0);
+		Assert.assertFalse(c.getAllPOI().size() == 0);
+		Assert.assertFalse(c.getAllPOIName().size() == 0);
+	}
+
+	@Test
+	public void testGetGraph() {
+		Assert.assertNotNull(ConstantData.getGraph());
+	}
+}
