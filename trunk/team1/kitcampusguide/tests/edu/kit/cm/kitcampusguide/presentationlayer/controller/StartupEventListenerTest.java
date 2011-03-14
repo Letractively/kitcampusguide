26dde588b7e572f8cefd2193b317ef0eaa8b0743
isabel.funke@student.kit.edu <isabel.funke@student.kit.edu@02b7392b-9286-25d4-34bb-9ed0ff935e76>
2011-03-14 13:06:11 +0000

    StartupEventListenerTest.java added - uses org.springframework.test.annotation.ExpectedException

---
+++ trunk/team1/kitcampusguide/tests/edu/kit/cm/kitcampusguide/presentationlayer/controller/StartupEventListenerTest.java
@@ -0,0 +1,61 @@
+package edu.kit.cm.kitcampusguide.presentationlayer.controller;
+
+import javax.faces.component.UICommand;
+import javax.faces.event.PostConstructApplicationEvent;
+import javax.faces.event.PostValidateEvent;
+
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import static org.junit.Assert.*;
+
+import org.apache.shale.test.base.AbstractJsfTestCase;
+import org.springframework.test.annotation.ExpectedException;
+
+public class StartupEventListenerTest extends AbstractJsfTestCase {
+	
+	StartupEventListener startupEventListener;
+	PostConstructApplicationEvent testEvent;
+	
+	public StartupEventListenerTest(String name) {
+		 super(name);
+	}
+	
+	public static Test suite() {
+	      return new TestSuite(StartupEventListenerTest.class);
+	  }
+	
+	public void setUp() throws Exception {
+	    super.setUp();
+	    
+	    startupEventListener = new StartupEventListener();	    	    
+	}
+	
+	public void tearDown() throws Exception {
+	    startupEventListener = null;	    
+		
+	    super.tearDown();
+	}
+	
+	public void testIsListenerForSource() {
+		assertTrue(startupEventListener.isListenerForSource(application));
+		assertFalse(startupEventListener.isListenerForSource("source"));
+	}
+
+	public void testProcessEvent_uninterestingEvent() {
+		
+		startupEventListener.processEvent(new PostValidateEvent(new UICommand())); 
+		//nothing to test here
+	}
+	
+	@ExpectedException(java.net.MalformedURLException.class)
+	public void testProcessEvent_postConstructApplicationEvent () {
+		testEvent = new PostConstructApplicationEvent(application);
+		startupEventListener.processEvent(testEvent);
+		//exception expected since no proper PostConstructApplicationEvent can be thrown when
+		//running this as JUnitTest
+	}
+	
+}
+
+
