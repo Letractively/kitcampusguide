9f957481d0a089726b2583fb983678c17aef6646
tobias.zuendorf@student.kit.edu <tobias.zuendorf@student.kit.edu@02b7392b-9286-25d4-34bb-9ed0ff935e76>
2011-03-29 21:48:17 +0000

    Laden des Graphen verändert

--- trunk/team2/src/edu/kit/cm/kitcampusguide/data/ConcreteMapLoader.java
+++ trunk/team2/src/edu/kit/cm/kitcampusguide/data/ConcreteMapLoader.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
+import edu.kit.cm.kitcampusguide.ConstantData;
 import edu.kit.cm.kitcampusguide.model.Graph;
 import edu.kit.cm.kitcampusguide.model.Point;
 
@@ -22,86 +23,88 @@ public class ConcreteMapLoader implements MapLoader {
 	 */
 	@Override
 	public Graph getGraph() {
-		ArrayList<Integer> streetnodeId = new ArrayList<Integer>();
-		ArrayList<Double> streetnodeX = new ArrayList<Double>();
-		ArrayList<Double> streetnodeY = new ArrayList<Double>();
-		
-		ArrayList<Integer> streetFrom = new ArrayList<Integer>();
-		ArrayList<Integer> streetTo = new ArrayList<Integer>();
-		ArrayList<Double> streetLength = new ArrayList<Double>();
-		
-		String dbURL = "jdbc:" + Config.dbType + "://" + Config.dbHost + ":" + Config.dbPort + "/" + Config.dbDatabase;
-		
-		Connection connection = null;
-		Statement statement = null;
-		ResultSet resultset = null;
-		
-		try {
-	        Class.forName("org.postgresql.Driver");
-	        connection = DriverManager.getConnection(dbURL, Config.dbUsername, Config.dbPassword);
-
-	        statement = connection.createStatement();
-	        resultset = statement.executeQuery("SELECT * FROM cg_streetnode");     
-	        
-	        while (resultset.next()) {
-	            streetnodeId.add(resultset.getInt(1));
-	            streetnodeX.add(resultset.getDouble(2));
-	            streetnodeY.add(resultset.getDouble(3));
+		if (ConstantData.getGraph() == null) {
+			ArrayList<Integer> streetnodeId = new ArrayList<Integer>();
+			ArrayList<Double> streetnodeX = new ArrayList<Double>();
+			ArrayList<Double> streetnodeY = new ArrayList<Double>();
+			
+			ArrayList<Integer> streetFrom = new ArrayList<Integer>();
+			ArrayList<Integer> streetTo = new ArrayList<Integer>();
+			ArrayList<Double> streetLength = new ArrayList<Double>();
+			
+			String dbURL = "jdbc:" + Config.dbType + "://" + Config.dbHost + ":" + Config.dbPort + "/" + Config.dbDatabase;
+			
+			Connection connection = null;
+			Statement statement = null;
+			ResultSet resultset = null;
+			
+			try {
+		        Class.forName("org.postgresql.Driver");
+		        connection = DriverManager.getConnection(dbURL, Config.dbUsername, Config.dbPassword);
+	
+		        statement = connection.createStatement();
+		        resultset = statement.executeQuery("SELECT * FROM cg_streetnode");     
+		        
+		        while (resultset.next()) {
+		            streetnodeId.add(resultset.getInt(1));
+		            streetnodeX.add(resultset.getDouble(2));
+		            streetnodeY.add(resultset.getDouble(3));
+				}
+		        
+		        
+		        statement = connection.createStatement();
+		        resultset = statement.executeQuery("SELECT * FROM cg_street");     
+		        
+		        while (resultset.next()) {
+		        	streetFrom.add(resultset.getInt(1));
+		        	streetTo.add(resultset.getInt(2));
+		        	streetLength.add(resultset.getDouble(3));
+				}
+	
+		        
+			} catch (Exception ex) {
+		        System.out.println( ex );
+		    } finally {
+		        try { if( null != resultset ) resultset.close(); } catch( Exception ex ) {}
+		        try { if( null != statement ) statement.close(); } catch( Exception ex ) {}
+		        try { if( null != connection ) connection.close(); } catch( Exception ex ) {}
+		    }
+		      
+		    Point[] points = new Point[streetnodeId.size()];
+		      
+		    for (int i = 0; i < streetnodeId.size(); i++) {
+		    	points[streetnodeId.get(i)] = new Point(streetnodeX.get(i), streetnodeY.get(i)); 
+		    }
+		      
+		      
+		    Integer[] vertices = new Integer[streetnodeId.size() + 1];
+		    Integer[] edges = new Integer[streetFrom.size()];
+		    Double[] lengths = new Double[streetFrom.size()];
+		    vertices[0] = 0;
+		     
+		    for (int i = 1; i < vertices.length; i++) {
+		    	vertices[i] = 0;
+		    }
+		      
+		    // calculate amount of edges of a node.
+			for (Integer from : streetFrom) {
+				vertices[from]++;
 			}
-	        
-	        
-	        statement = connection.createStatement();
-	        resultset = statement.executeQuery("SELECT * FROM cg_street");     
-	        
-	        while (resultset.next()) {
-	        	streetFrom.add(resultset.getInt(1));
-	        	streetTo.add(resultset.getInt(2));
-	        	streetLength.add(resultset.getDouble(3));
+			  
+			// calculate offsets.
+			for (int i = 1; i < vertices.length; i++) {
+				vertices[i] += vertices[i - 1];
 			}
-
-	        
-	      } catch (Exception ex) {
-	          System.out.println( ex );
-	      } finally {
-	          try { if( null != resultset ) resultset.close(); } catch( Exception ex ) {}
-	          try { if( null != statement ) statement.close(); } catch( Exception ex ) {}
-	          try { if( null != connection ) connection.close(); } catch( Exception ex ) {}
-	      }
-	      
-	      Point[] points = new Point[streetnodeId.size()];
-	      
-	      for (int i = 0; i < streetnodeId.size(); i++) {
-	    	  points[streetnodeId.get(i)] = new Point(streetnodeX.get(i), streetnodeY.get(i)); 
-	      }
-	      
-	      
-	      Integer[] vertices = new Integer[streetnodeId.size() + 1];
-	      Integer[] edges = new Integer[streetFrom.size()];
-	      Double[] lengths = new Double[streetFrom.size()];
-	      vertices[0] = 0;
-	     
-	      for (int i = 1; i < vertices.length; i++) {
-	    	  vertices[i] = 0;
-	      }
-	      
-	      // calculate amount of edges of a node.
-		  for (Integer from : streetFrom) {
-		      vertices[from]++;
-		  }
-		  
-		  // calculate offsets.
-		  for (int i = 1; i < vertices.length; i++) {
-			  vertices[i] += vertices[i - 1];
-		  }
-	      
-		  for (int i = 0; i < streetFrom.size(); i++) {
-			  edges[--vertices[streetFrom.get(i)]] = streetTo.get(i);
-			  lengths[vertices[streetFrom.get(i)]] = streetLength.get(i);
-		  }
-		  
-		 
-		  Graph graph = new Graph(points, lengths, vertices, edges);
-	      return graph;
+		      
+			for (int i = 0; i < streetFrom.size(); i++) {
+				edges[--vertices[streetFrom.get(i)]] = streetTo.get(i);
+				lengths[vertices[streetFrom.get(i)]] = streetLength.get(i);
+			}
+			  
+			 
+			ConstantData.setGraph(new Graph(points, lengths, vertices, edges));
+		}
+	    return ConstantData.getGraph();
 	}
 	
 	/**
