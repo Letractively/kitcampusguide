63a19314f59466ba02a6eba7c537cc282e09d228
roland.steinegger <roland.steinegger@student.kit.edu>
2011-10-07 10:01:57 +0000

    extracted security module for permission evaluation and in memory member control webservice call from client to service including web service security with wss4j and spring ws

--- kitcampusguide/poi-manager/webapp/src/main/java/edu/kit/cm/kitcampusguide/controller/form/UpdatePoiForm.java
+++ kitcampusguide/poi-manager/webapp/src/main/java/edu/kit/cm/kitcampusguide/controller/form/UpdatePoiForm.java
@@ -1,5 +1,7 @@
 package edu.kit.cm.kitcampusguide.controller.form;
 
+import java.util.Collection;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,100 +13,112 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import edu.kit.cm.kitcampusguide.model.Group;
+import edu.kit.cm.kitcampusguide.service.user.MemberService;
 import edu.kit.cm.kitcampusguide.validator.PoiValidator;
-import edu.kit.cm.kitcampusguide.ws.poi.PoiService;
-import edu.kit.cm.kitcampusguide.ws.poi.PoiServiceClient;
+import edu.kit.cm.kitcampusguide.ws.poi.PoiFacade;
+import edu.kit.cm.kitcampusguide.ws.poi.type.ExecuteFault;
 import edu.kit.cm.kitcampusguide.ws.poi.type.PoiWithId;
-import edu.kit.cm.kitcampusguide.ws.poi.type.ReadFault_Exception;
-import edu.kit.cm.kitcampusguide.ws.poi.type.ReadRequest;
-import edu.kit.cm.kitcampusguide.ws.poi.type.UpdateFault_Exception;
-import edu.kit.cm.kitcampusguide.ws.poi.type.UpdateRequest;
+import edu.kit.cm.kitcampusguide.ws.poi.type.ReadRequestComplexType;
+import edu.kit.cm.kitcampusguide.ws.poi.type.UpdateRequestComplexType;
 
+/**
+ * Controller for updating poi webpage.
+ * 
+ * @author Roland Steinegger, Karlsruhe Institute of Technology
+ */
 @Controller
 @RequestMapping("poi/{uid}/update.htm")
 @SessionAttributes(types = PoiWithId.class)
 public class UpdatePoiForm {
 
-	private static final Logger log = Logger.getLogger(UpdatePoiForm.class);
+    private static final Logger log = Logger.getLogger(UpdatePoiForm.class);
 
-	@Autowired
-	private PoiService poiService;
+    @Autowired
+    private PoiFacade poiFacade;
 
-	public UpdatePoiForm() {
-		// autowiring constructor
-	}
+    @Autowired
+    private MemberService memberService;
 
-	public UpdatePoiForm(PoiService poiService) {
-		this.poiService = poiService;
-	}
+    public UpdatePoiForm() {
+        super();
+        // autowiring constructor
+    }
 
-	@RequestMapping(method = RequestMethod.GET)
-	protected String setUpForm(@PathVariable("uid") int uid, Model model) {
-		String viewName;
+    public UpdatePoiForm(PoiFacade poiFacade, MemberService memberService) {
+        super();
+        this.poiFacade = poiFacade;
+        this.memberService = memberService;
+    }
 
-		try {
+    @RequestMapping(method = RequestMethod.GET)
+    protected String setUpForm(@PathVariable("uid") int uid, Model model) {
+        log.debug("Setting up form for updating poi with id " + uid);
+        String viewName;
 
-			tryToLoadPoi(uid, model);
-			viewName = "poi/form";
-		} catch (ReadFault_Exception e) {
+        try {
 
-			handleReadExceptionAndSetErrorView(model, e);
-			viewName = "redirect:/pois.htm";
-		}
+            tryToLoadPoiAndSetToModel(uid, model);
+            tryToLoadGroupsAndSetToModel(model);
+            viewName = "poi/form";
+        } catch (ExecuteFault e) {
 
-		return viewName;
-	}
+            handleReadExceptionAndSetErrorView(model, e);
+            viewName = "redirect:/pois.htm";
+        }
 
-	private void tryToLoadPoi(int uid, Model model) throws ReadFault_Exception {
-		ReadRequest readRequest = new ReadRequest();
-		readRequest.setId(uid);
-		model.addAttribute("poi", getServiceClient().read(readRequest).getPoi());
-	}
+        return viewName;
+    }
 
-	private void handleReadExceptionAndSetErrorView(Model model, ReadFault_Exception e) {
-		log.error("Reading a poi failed.", e);
-		model.addAttribute("errorMessage", "error.readPoi");
-	}
+    private void tryToLoadPoiAndSetToModel(int uid, Model model) throws ExecuteFault {
+        ReadRequestComplexType readRequest = new ReadRequestComplexType();
+        readRequest.setId(uid);
+        PoiWithId poi = (poiFacade.read(readRequest).getPoi());
+        model.addAttribute("poi", poi);
+    }
 
-	@RequestMapping(method = RequestMethod.POST)
-	protected String onSubmit(@ModelAttribute(value = "poi") PoiWithId poi, BindingResult result, Model model) {
-		String viewName;
-		new PoiValidator().validate(poi, result);
-		if (result.hasErrors()) {
+    private void tryToLoadGroupsAndSetToModel(Model model) {
+        Collection<Group> groups = this.memberService.getGroups();
+        model.addAttribute("groups", groups);
+    }
 
-			viewName = "poi/form";
-		} else {
-			try {
+    private void handleReadExceptionAndSetErrorView(Model model, ExecuteFault e) {
+        log.error("Reading a poi failed.", e);
+        model.addAttribute("errorMessage", "error.readPoi");
+    }
 
-				tryToUpdatePoiAndAddSuccessMessage(poi, model);
-				viewName = "redirect:/pois.htm";
-			} catch (UpdateFault_Exception e) {
+    @RequestMapping(method = RequestMethod.POST)
+    protected String onSubmit(@ModelAttribute(value = "poi") PoiWithId poi, BindingResult result, Model model) {
+        log.debug("Update request for poi. data: " + poi);
+        String viewName;
+        new PoiValidator().validate(poi, result);
+        if (result.hasErrors()) {
 
-				handleUpdateException(model, e);
-				viewName = "poi/form";
-			}
-		}
+            viewName = "poi/form";
+        } else {
+            try {
 
-		return viewName;
-	}
+                tryToUpdatePoiAndAddSuccessMessage(poi, model);
+                viewName = "redirect:/pois.htm";
+            } catch (ExecuteFault e) {
 
-	private void handleUpdateException(Model model, UpdateFault_Exception e) {
-		log.error("Error while updating POI", e);
-		model.addAttribute("faultMessage", "error.updatingPoi");
-	}
+                handleUpdateException(model, e);
+                viewName = "poi/form";
+            }
+        }
 
-	private void tryToUpdatePoiAndAddSuccessMessage(PoiWithId poi, Model model) throws UpdateFault_Exception {
-		UpdateRequest updateRequest = new UpdateRequest();
-		updateRequest.setPoi(poi);
-		getServiceClient().update(updateRequest);
-		model.addAttribute("successMessage", "success.updatingPoi");
-	}
+        return viewName;
+    }
 
-	private PoiService getServiceClient() {
-		if (this.poiService != null) {
-			return this.poiService;
-		} else {
-			return new PoiServiceClient().getPoiServiceSOAP();
-		}
-	}
+    private void handleUpdateException(Model model, ExecuteFault e) {
+        log.error("Error while updating POI", e);
+        model.addAttribute("faultMessage", "error.updatingPoi");
+    }
+
+    private void tryToUpdatePoiAndAddSuccessMessage(PoiWithId poi, Model model) throws ExecuteFault {
+        UpdateRequestComplexType updateRequest = new UpdateRequestComplexType();
+        updateRequest.setPoi(poi);
+        poiFacade.update(updateRequest);
+        model.addAttribute("successMessage", "success.updatingPoi");
+    }
 }
--- kitcampusguide/poi-manager/webapp/src/main/java/edu/kit/cm/kitcampusguide/validator/PoiValidator.java
+++ kitcampusguide/poi-manager/webapp/src/main/java/edu/kit/cm/kitcampusguide/validator/PoiValidator.java
@@ -1,82 +1,87 @@
 package edu.kit.cm.kitcampusguide.validator;
 
+import org.apache.log4j.Logger;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
+import edu.kit.cm.kitcampusguide.controller.form.CreatePoiForm;
 import edu.kit.cm.kitcampusguide.ws.poi.type.Poi;
 import edu.kit.cm.kitcampusguide.ws.poi.type.PoiWithId;
 
 public class PoiValidator implements Validator {
 
-	private final Double DEFAULT_MAX_LONGITUDE = 180d;
-	private final Double DEFAULT_MIN_LONGITUDE = -180d;
-	private final Double maxLongitude = DEFAULT_MAX_LONGITUDE;
-	private final Double minLongitude = DEFAULT_MIN_LONGITUDE;
-	private final Double DEFAULT_MAX_LATITUDE = 180d;
-	private final Double DEFAULT_MIN_LATITUDE = -180d;
-	private final Double maxLatitude = DEFAULT_MAX_LATITUDE;
-	private final Double minLatitude = DEFAULT_MIN_LATITUDE;
+    private static final Logger log = Logger.getLogger(CreatePoiForm.class);
 
-	@Override
-	public boolean supports(Class<?> arg0) {
-		return arg0.equals(PoiWithId.class) || arg0.equals(Poi.class);
-	}
+    private final Double DEFAULT_MAX_LONGITUDE = 180d;
+    private final Double DEFAULT_MIN_LONGITUDE = -180d;
+    private final Double maxLongitude = DEFAULT_MAX_LONGITUDE;
+    private final Double minLongitude = DEFAULT_MIN_LONGITUDE;
+    private final Double DEFAULT_MAX_LATITUDE = 180d;
+    private final Double DEFAULT_MIN_LATITUDE = -180d;
+    private final Double maxLatitude = DEFAULT_MAX_LATITUDE;
+    private final Double minLatitude = DEFAULT_MIN_LATITUDE;
 
-	@Override
-	public void validate(Object obj, Errors errors) {
-		if (obj instanceof Poi) {
+    @Override
+    public boolean supports(Class<?> arg0) {
+        return arg0.equals(PoiWithId.class) || arg0.equals(Poi.class);
+    }
 
-			validate((Poi) obj, errors);
-		} else if (obj instanceof PoiWithId) {
+    @Override
+    public void validate(Object obj, Errors errors) {
+        log.debug("Processing object(" + obj.toString() + ") in poi validator.");
+        if (obj instanceof Poi) {
 
-			validate(Poi.convertToPoi((PoiWithId) obj), errors);
-		} else {
+            validate((Poi) obj, errors);
+        } else if (obj instanceof PoiWithId) {
 
-			errors.reject("error.classNotSupported");
-		}
-	}
+            validate(Poi.convertToPoi((PoiWithId) obj), errors);
+        } else {
 
-	public void validate(Poi poi, Errors errors) {
-		if (poi == null) {
+            errors.reject("error.classNotSupported");
+        }
+    }
 
-			errors.reject("error.poiNotSpecified");
-		} else {
+    public void validate(Poi poi, Errors errors) {
+        if (poi == null) {
 
-			ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "error.nameNotSpecified", "Name required");
-			ValidationUtils.rejectIfEmptyOrWhitespace(errors, "description", "error.descriptionNotSpecified",
-					"Description required");
-			ValidationUtils.rejectIfEmptyOrWhitespace(errors, "longitude", "error.longitudeNotSpecified",
-					"Longitude required");
-			ValidationUtils.rejectIfEmptyOrWhitespace(errors, "latitude", "error.latitudeNotSpecified",
-					"Latitude required");
+            errors.reject("error.poiNotSpecified");
+        } else {
 
-			rejectIfLatitudeOutOfRange(errors, poi);
-			rejectIfLongitudeOutOfRange(errors, poi);
-		}
-	}
+            ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "error.nameNotSpecified", "Name required");
+            ValidationUtils.rejectIfEmptyOrWhitespace(errors, "description", "error.descriptionNotSpecified",
+                    "Description required");
+            ValidationUtils.rejectIfEmptyOrWhitespace(errors, "longitude", "error.longitudeNotSpecified",
+                    "Longitude required");
+            ValidationUtils.rejectIfEmptyOrWhitespace(errors, "latitude", "error.latitudeNotSpecified",
+                    "Latitude required");
 
-	private void rejectIfLatitudeOutOfRange(Errors errors, Poi poi) {
-		if (poi.getLatitude() > maxLatitude) {
+            rejectIfLatitudeOutOfRange(errors, poi);
+            rejectIfLongitudeOutOfRange(errors, poi);
+        }
+    }
 
-			errors.rejectValue("latitude", "errors.latitudeTooHigh", new Object[] { new Double(maxLatitude) },
-					"Latitude too high");
-		} else if (poi.getLatitude() < minLatitude) {
+    private void rejectIfLatitudeOutOfRange(Errors errors, Poi poi) {
+        if (poi.getLatitude() > maxLatitude) {
 
-			errors.rejectValue("latitude", "errors.latitudeTooLow", new Object[] { new Double(minLatitude) },
-					"Latitude too low");
-		}
-	}
+            errors.rejectValue("latitude", "errors.latitudeTooHigh", new Object[] { new Double(maxLatitude) },
+                    "Latitude too high");
+        } else if (poi.getLatitude() < minLatitude) {
 
-	private void rejectIfLongitudeOutOfRange(Errors errors, Poi poi) {
-		if (poi.getLongitude() > maxLongitude) {
+            errors.rejectValue("latitude", "errors.latitudeTooLow", new Object[] { new Double(minLatitude) },
+                    "Latitude too low");
+        }
+    }
 
-			errors.rejectValue("longitude", "errors.longitudeTooHigh", new Object[] { new Double(maxLongitude) },
-					"Longitude too high");
-		} else if (poi.getLongitude() < minLongitude) {
+    private void rejectIfLongitudeOutOfRange(Errors errors, Poi poi) {
+        if (poi.getLongitude() > maxLongitude) {
 
-			errors.rejectValue("longitude", "errors.longitudeTooLow", new Object[] { new Double(minLongitude) },
-					"Longitude too low");
-		}
-	}
+            errors.rejectValue("longitude", "errors.longitudeTooHigh", new Object[] { new Double(maxLongitude) },
+                    "Longitude too high");
+        } else if (poi.getLongitude() < minLongitude) {
+
+            errors.rejectValue("longitude", "errors.longitudeTooLow", new Object[] { new Double(minLongitude) },
+                    "Longitude too low");
+        }
+    }
 }
