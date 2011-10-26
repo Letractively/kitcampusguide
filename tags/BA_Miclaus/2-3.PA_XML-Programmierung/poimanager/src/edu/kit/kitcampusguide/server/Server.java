29508d58af96612af2cbcbd722f1dac3e02160e2
aleksander.dikanski@kit.edu <aleksander.dikanski@kit.edu@02b7392b-9286-25d4-34bb-9ed0ff935e76>
2011-10-26 15:16:52 +0000

    INFORMATION: PA 2-3 XML-Programmierung

---
+++ tags/BA_Miclaus/2-3.PA_XML-Programmierung/poimanager/src/edu/kit/kitcampusguide/server/Server.java
@@ -0,0 +1,7 @@
+package edu.kit.kitcampusguide.server;
+
+public interface Server {
+
+	public abstract void start();
+
+}
---
+++ tags/BA_Miclaus/2-3.PA_XML-Programmierung/poimanager/test/edu/kit/kitcampusguide/poi/dataaccess/TestSaxReader.java
@@ -0,0 +1,46 @@
+package edu.kit.kitcampusguide.poi.dataaccess;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.kit.kitcampusguide.poi.model.POI;
+
+public class TestSaxReader {
+	
+	private static final double DELTA = 0.0001;
+
+	private String file = "./Data/POIList.xml";
+	
+	private SaxReader reader;
+
+	@Before
+	public void setUp() throws Exception {
+		reader = new SaxReader();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testReadAllPOIs() {
+		List<POI> poiList = reader.readAllPOIs(file);
+		
+		assertNotNull(poiList);
+		assertFalse(poiList.isEmpty());
+		assertEquals(3, poiList.size());
+		POI p = poiList.get(0);
+		
+		assertNotNull(p);
+		assertEquals(2323,p.getId());
+		assertEquals("Mensa am Adenaurerring", p.getName());
+		assertEquals(49.011924, p.getLatitude(), DELTA);
+		assertEquals(8.41678, p.getLongitude(), DELTA);
+	}
+
+}
