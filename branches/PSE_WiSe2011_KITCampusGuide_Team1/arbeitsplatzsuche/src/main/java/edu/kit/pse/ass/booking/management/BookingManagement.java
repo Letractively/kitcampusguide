6f46fc5d24ef70749c1f634cd7c1f27046ca2169
fabian.stein@student.kit.edu <fabian.stein@student.kit.edu@02b7392b-9286-25d4-34bb-9ed0ff935e76>
2011-12-21 10:50:24 +0000

    fixed ONE syntax error

--- branches/PSE_WiSe2011_KITCampusGuide_Team1/arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/booking/management/BookingManagement.java
+++ branches/PSE_WiSe2011_KITCampusGuide_Team1/arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/booking/management/BookingManagement.java
@@ -11,91 +11,120 @@ import edu.kit.pse.ass.entity.Reservation;
 // TODO: Auto-generated Javadoc
 /**
  * The Interface BookingManagement.
- *
+ * 
  * @author Andreas Bosch
  */
 public interface BookingManagement {
-    
-    /**
-     * Book.
-     *
-     * @param userID the user id
-     * @param facilityIDs the facility i ds
-     * @param startDate the start date
-     * @param endDate the end date
-     * @return the string
-     */
-    public String book(String userID, Collection<String> facilityIDs, Date startDate, Date endDate);
 
-    /**
-     * List reservations of user.
-     *
-     * @param userID the user id
-     * @param asFrom the as from
-     * @param upTo the up to
-     * @return the collection
-     */
-    public Collection<Reservation> listReservationsOfUser(String userID, Date asFrom, Date upTo);
-    
-    /**
-     * List reservations of facility.
-     *
-     * @param facilityID the facility id
-     * @param asFrom the as from
-     * @param upTo the up to
-     * @return the collection
-     */
-    public Collection<Reservation> listReservationsOfFacility(String facilityID, Date asFrom, Date upTo);
+	/**
+	 * Book.
+	 * 
+	 * @param userID
+	 *            the user id
+	 * @param facilityIDs
+	 *            the facility i ds
+	 * @param startDate
+	 *            the start date
+	 * @param endDate
+	 *            the end date
+	 * @return the string
+	 */
+	public String book(String userID, Collection<String> facilityIDs,
+			Date startDate, Date endDate);
 
-    /**
-     * Change reservation end.
-     *
-     * @param reservationID the reservation id
-     * @param newEndDate the new end date
-     */
-    public void changeReservationEnd(String reservationID, Date newEndDate);
-    
-    /**
-     * Removes the facility from reservation.
-     *
-     * @param reservationID the reservation id
-     * @param facilityID the facility id
-     */
-    public void removeFacilityFromReservation(String reservationID, String facilityID);
-    
-    /**
-     * Delete reservation.
-     *
-     * @param reservationID the reservation id
-     */
-    public void deleteReservation(String reservationID)
-    
-    /**
-     * Gets the reservation.
-     *
-     * @param reservationID the reservation id
-     * @return the reservation
-     */
-    public Reservation getReservation(String reservationID);
+	/**
+	 * List reservations of user.
+	 * 
+	 * @param userID
+	 *            the user id
+	 * @param asFrom
+	 *            the as from
+	 * @param upTo
+	 *            the up to
+	 * @return the collection
+	 */
+	public Collection<Reservation> listReservationsOfUser(String userID,
+			Date asFrom, Date upTo);
 
-    /**
-     * Find free facilites.
-     *
-     * @param query the query
-     * @param start the start
-     * @param end the end
-     * @param fullyAvailible the fully availible
-     * @return the collection
-     */
-    public Collection<FreeFacilityResult> findFreeFacilites(FacilityQuery query, Date start, Date end, boolean fullyAvailible);
-    
-    /**
-     * Checks if is facility free.
-     *
-     * @param facilityID the facility id
-     * @param startDate the start date
-     * @param endDate the end date
-     * @return true, if is facility free
-     */
-    public boolean isFacilityFree(String facilityID, Date startDate, Date endDate);
+	/**
+	 * List reservations of facility.
+	 * 
+	 * @param facilityID
+	 *            the facility id
+	 * @param asFrom
+	 *            the as from
+	 * @param upTo
+	 *            the up to
+	 * @return the collection
+	 */
+	public Collection<Reservation> listReservationsOfFacility(
+			String facilityID, Date asFrom, Date upTo);
+
+	/**
+	 * Change reservation end.
+	 * 
+	 * @param reservationID
+	 *            the reservation id
+	 * @param newEndDate
+	 *            the new end date
+	 */
+	public void changeReservationEnd(String reservationID, Date newEndDate);
+
+	/**
+	 * Removes the facility from reservation.
+	 * 
+	 * @param reservationID
+	 *            the reservation id
+	 * @param facilityID
+	 *            the facility id
+	 */
+	public void removeFacilityFromReservation(String reservationID,
+			String facilityID);
+
+	/**
+	 * Delete reservation.
+	 * 
+	 * @param reservationID
+	 *            the reservation id
+	 */
+	public void deleteReservation(String reservationID);
+
+	/**
+	 * Gets the reservation.
+	 * 
+	 * @param reservationID
+	 *            the reservation id
+	 * @return the reservation
+	 */
+	public Reservation getReservation(String reservationID);
+
+	/**
+	 * Find free facilites.
+	 * 
+	 * @param query
+	 *            the query
+	 * @param start
+	 *            the start
+	 * @param end
+	 *            the end
+	 * @param fullyAvailible
+	 *            the fully availible
+	 * @return the collection
+	 */
+	public Collection<FreeFacilityResult> findFreeFacilites(
+			FacilityQuery query, Date start, Date end, boolean fullyAvailible);
+
+	/**
+	 * Checks if is facility free.
+	 * 
+	 * @param facilityID
+	 *            the facility id
+	 * @param startDate
+	 *            the start date
+	 * @param endDate
+	 *            the end date
+	 * @return true, if is facility free
+	 */
+	public boolean isFacilityFree(String facilityID, Date startDate,
+			Date endDate);
 }
