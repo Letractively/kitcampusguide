e52bc34d7dab39098a613ab23314f38cc20079ad
ufddz@student.kit.edu <ufddz@student.kit.edu@02b7392b-9286-25d4-34bb-9ed0ff935e76>
2011-12-21 14:32:55 +0000

    Created BookingDAOImpl with method dummies

---
+++ branches/PSE_WiSe2011_KITCampusGuide_Team1/arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/booking/dao/BookingDAOImpl.java
@@ -0,0 +1,42 @@
+package edu.kit.pse.ass.booking.dao;
+
+import java.util.Collection;
+import java.util.Date;
+
+import edu.kit.pse.ass.entity.Reservation;
+
+public class BookingDAOImpl implements BookingDAO {
+
+	public Collection<Reservation> getReservationsOfUser(String userID,
+			Date asFrom, Date upTo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Collection<Reservation> getReservationsOfFacility(String facilityID,
+			Date asFrom, Date upTo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Reservation getReservation(String reservationID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String insertReservation(Reservation reservation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void updateReservation(Reservation reservation) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void deleteReservation(String reservationID) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
