1764f605c3f5d8db8c1b728e5696fab13d759c6e
ufddz <ufddz@student.kit.edu>
2011-12-21 15:04:52 +0000

    Added methods of the FacilityManagement interface with JavaDoc comments, created FacilityManagementImpl with method dummies

---
+++ arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/facility/management/FacilityManagementImpl.java
@@ -0,0 +1,25 @@
+package edu.kit.pse.ass.facility.management;
+
+import java.util.Collection;
+
+import edu.kit.pse.ass.entity.Facility;
+
+public class FacilityManagementImpl implements FacilityManagement {
+
+	public Facility getFacility(String facilityID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Collection<Facility> findMatchingFacilities(
+			FacilityQuery facilityQuery) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Collection<Facility> getAvailablePropertiesOf(Class facilityClass) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
