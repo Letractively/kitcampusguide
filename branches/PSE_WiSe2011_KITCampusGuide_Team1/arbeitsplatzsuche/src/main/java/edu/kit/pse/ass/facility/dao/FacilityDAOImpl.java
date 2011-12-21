b5d1cb812feba15ad6970b35872d5dd7255e38cc
ufddz@student.kit.edu <ufddz@student.kit.edu@02b7392b-9286-25d4-34bb-9ed0ff935e76>
2011-12-21 14:43:25 +0000

    Added methods of the FacilityDAO interface with JavaDoc comments, created FacilityDAOImpl with method dummies

---
+++ branches/PSE_WiSe2011_KITCampusGuide_Team1/arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/facility/dao/FacilityDAOImpl.java
@@ -0,0 +1,25 @@
+package edu.kit.pse.ass.facility.dao;
+
+import java.util.Collection;
+
+import edu.kit.pse.ass.entity.Facility;
+import edu.kit.pse.ass.entity.Property;
+
+public class FacilityDAOImpl implements FacilityDAO {
+
+	public Facility getFacility(String facilityID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Collection<Facility> getFacilities(Collection<Property> properties) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Collection<Property> getAvailablePropertiesOf(Class facilityClass) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
