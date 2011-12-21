5954c60f1ec2b67d67d849760f7b59079f1ac6bc
ufddz@student.kit.edu <ufddz@student.kit.edu@02b7392b-9286-25d4-34bb-9ed0ff935e76>
2011-12-21 15:04:52 +0000

    Added methods of the FacilityManagement interface with JavaDoc comments, created FacilityManagementImpl with method dummies

--- branches/PSE_WiSe2011_KITCampusGuide_Team1/arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/facility/management/FacilityManagement.java
+++ branches/PSE_WiSe2011_KITCampusGuide_Team1/arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/facility/management/FacilityManagement.java
@@ -3,10 +3,44 @@
  */
 package edu.kit.pse.ass.facility.management;
 
+import java.util.Collection;
+
+import edu.kit.pse.ass.entity.Facility;
+
 /**
  * @author Andreas Bosch
- *
+ * 
  */
 public interface FacilityManagement {
 
+	/**
+	 * returns facility with given ID
+	 * 
+	 * @param facilityID
+	 *            the ID of the facility
+	 * @return facility with given ID
+	 */
+	public Facility getFacility(String facilityID);
+
+	/**
+	 * returns a collection of facilities matching the criteria of the given
+	 * facilityQuery
+	 * 
+	 * @param facilityQuery
+	 *            the facility query
+	 * @return a collection of matching facilities
+	 */
+	public Collection<Facility> findMatchingFacilities(
+			FacilityQuery facilityQuery);
+
+	/**
+	 * returns a collection of properties that an object of the given class can
+	 * have
+	 * 
+	 * @param facilityClass
+	 *            the class
+	 * @return collection of properties that an object of the given class can
+	 *         have
+	 */
+	public Collection<Facility> getAvailablePropertiesOf(Class facilityClass);
 }
