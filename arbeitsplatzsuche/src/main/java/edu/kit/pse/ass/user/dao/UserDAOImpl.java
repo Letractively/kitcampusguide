2c8c84c941707570ec782d3861cd15d34dc88e5f
ufddz <ufddz@student.kit.edu>
2011-12-21 18:22:24 +0000

    Created UserDAO interface and UserDAOImpl with method dummies

---
+++ arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/user/dao/UserDAOImpl.java
@@ -0,0 +1,22 @@
+package edu.kit.pse.ass.user.dao;
+
+import edu.kit.pse.ass.entity.User;
+
+public class UserDAOImpl implements UserDAO {
+
+	public User insertUser(String userID, String passwordHash) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void deleteUser(String userID) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public User getUser(String userID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
