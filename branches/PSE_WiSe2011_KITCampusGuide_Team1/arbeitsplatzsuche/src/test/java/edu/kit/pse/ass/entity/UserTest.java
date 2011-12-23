3fc67420ef8ba0e921bf4178f3f59940fad3021a
oliver.schneider2@student.kit.edu <oliver.schneider2@student.kit.edu@02b7392b-9286-25d4-34bb-9ed0ff935e76>
2011-12-23 08:55:21 +0000

    Added Test for User

---
+++ branches/PSE_WiSe2011_KITCampusGuide_Team1/arbeitsplatzsuche/src/test/java/edu/kit/pse/ass/entity/UserTest.java
@@ -0,0 +1,45 @@
+package edu.kit.pse.ass.entity;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserTest {
+
+	User user;
+	public static String TEST_ID = "1";
+	public static String TEST_ROLE = "student";
+	public static String TEST_EMAIL = "abc@student.kit.edu";
+	public static String TEST_PW = "pwWithMin8Chars";
+
+	@Before
+	public void setUp() throws Exception {
+		user = new User();
+	}
+
+	@Test
+	public void testSetId() {
+		user.setId(TEST_ID);
+		assertEquals(TEST_ID, user.getId());
+	}
+
+	@Test
+	public void testSetRole() {
+		user.setRole(TEST_ROLE);
+		assertEquals(TEST_ROLE, user.getRole());
+	}
+
+	@Test
+	public void testSetEmail() {
+		user.setEmail(TEST_EMAIL);
+		assertEquals(TEST_EMAIL, user.getEmail());
+	}
+
+	@Test
+	public void testSetPassword() {
+		user.setPassword(TEST_PW);
+		assertEquals(TEST_PW, user.getPassword());
+	}
+
+}
