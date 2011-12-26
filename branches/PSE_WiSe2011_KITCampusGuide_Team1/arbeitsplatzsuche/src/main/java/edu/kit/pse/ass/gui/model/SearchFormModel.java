89db2c34842c899ed00ef65326d186cacb81e643
oliver.schneider2@student.kit.edu <oliver.schneider2@student.kit.edu@02b7392b-9286-25d4-34bb-9ed0ff935e76>
2011-12-26 21:04:20 +0000

    added searchform model

---
+++ branches/PSE_WiSe2011_KITCampusGuide_Team1/arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/gui/model/SearchFormModel.java
@@ -0,0 +1,56 @@
+package edu.kit.pse.ass.gui.model;
+
+import java.util.Date;
+
+/**
+ * 
+ * @author Oliver Schneider
+ * 
+ */
+public class SearchFormModel {
+	private int workplaceCount;
+	private Date start;
+	private String duration;
+	private boolean wholeRoom;
+	private String searchText;
+
+	public int getWorkplaceCount() {
+		return workplaceCount;
+	}
+
+	public void setWorkplaceCount(int workplaceCount) {
+		this.workplaceCount = workplaceCount;
+	}
+
+	public Date getStart() {
+		return start;
+	}
+
+	public void setStart(Date start) {
+		this.start = start;
+	}
+
+	public String getDuration() {
+		return duration;
+	}
+
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+
+	public boolean isWholeRoom() {
+		return wholeRoom;
+	}
+
+	public void setWholeRoom(boolean wholeRoom) {
+		this.wholeRoom = wholeRoom;
+	}
+
+	public String getSearchText() {
+		return searchText;
+	}
+
+	public void setSearchText(String searchText) {
+		this.searchText = searchText;
+	}
+}
