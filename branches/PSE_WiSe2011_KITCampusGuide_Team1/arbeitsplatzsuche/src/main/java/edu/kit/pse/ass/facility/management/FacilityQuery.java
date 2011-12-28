9f188dc9cffe28b55f97b3440919a23ceddfdb1d
andreas.bosch@student.kit.edu <andreas.bosch@student.kit.edu@02b7392b-9286-25d4-34bb-9ed0ff935e76>
2011-12-28 16:10:31 +0000

    added getters for query parameters

--- branches/PSE_WiSe2011_KITCampusGuide_Team1/arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/facility/management/FacilityQuery.java
+++ branches/PSE_WiSe2011_KITCampusGuide_Team1/arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/facility/management/FacilityQuery.java
@@ -6,10 +6,23 @@ import edu.kit.pse.ass.entity.Property;
 
 public abstract class FacilityQuery {
 
+	private final Collection<Property> properties;
+	private final String searchText;
+
 	protected FacilityQuery(Collection<Property> properties, String searchText) {
+		this.properties = properties;
+		this.searchText = searchText;
 	}
 
 	protected FacilityFinder createFinder() {
 		return null;
+	}
+
+	public Collection<Property> getProperties() {
+		return this.properties;
+	}
+
+	public String getSearchText() {
+		return this.searchText;
 	}
 }
