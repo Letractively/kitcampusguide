8e789a1ea363a4e10479fb93c0bed19fee125d75
andreas.bosch <andreas.bosch@student.kit.edu>
2011-12-28 16:10:31 +0000

    added getters for query parameters

--- arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/facility/management/RoomQuery.java
+++ arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/facility/management/RoomQuery.java
@@ -6,10 +6,16 @@ import edu.kit.pse.ass.entity.Property;
 
 public class RoomQuery extends FacilityQuery {
 
+	protected int minimumWorkplaces;
+
 	public RoomQuery(Collection<Property> properties, String searchText,
 			int minimumWorkplaces) {
 		super(properties, searchText);
-		// TODO Auto-generated constructor stub
+		this.minimumWorkplaces = minimumWorkplaces;
+	}
+
+	public int getMinimumWorkplaces() {
+		return this.minimumWorkplaces;
 	}
 
 }
