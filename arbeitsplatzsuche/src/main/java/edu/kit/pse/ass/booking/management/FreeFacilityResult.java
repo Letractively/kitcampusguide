5df20d2c07ff1d81b6908b9154341b1eabe884a8
ufddz <ufddz@student.kit.edu>
2011-12-28 13:40:18 +0000

    Implemented FreeFacilityResult attributes, setters and getters

--- arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/booking/management/FreeFacilityResult.java
+++ arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/booking/management/FreeFacilityResult.java
@@ -1,5 +1,33 @@
 package edu.kit.pse.ass.booking.management;
 
+import java.util.Date;
+
+import edu.kit.pse.ass.entity.Facility;
+
 public class FreeFacilityResult {
-	// TODO: Implement
+
+	Facility facility;
+
+	Date start;
+
+	public FreeFacilityResult(Facility facility, Date start) {
+		this.facility = facility;
+		this.start = start;
+	}
+
+	public Facility getFacility() {
+		return facility;
+	}
+
+	public void setFacility(Facility facility) {
+		this.facility = facility;
+	}
+
+	public Date getStart() {
+		return start;
+	}
+
+	public void setStart(Date start) {
+		this.start = start;
+	}
 }
