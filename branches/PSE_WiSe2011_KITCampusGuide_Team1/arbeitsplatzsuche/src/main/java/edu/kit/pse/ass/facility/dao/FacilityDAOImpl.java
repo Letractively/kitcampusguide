ca6e20cc7d417f0d4452f68b608f6e978cedec54
andreas.bosch@student.kit.edu <andreas.bosch@student.kit.edu@02b7392b-9286-25d4-34bb-9ed0ff935e76>
2011-12-28 16:09:23 +0000

    implemented finding facilities with specific properties

--- branches/PSE_WiSe2011_KITCampusGuide_Team1/arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/facility/dao/FacilityDAOImpl.java
+++ branches/PSE_WiSe2011_KITCampusGuide_Team1/arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/facility/dao/FacilityDAOImpl.java
@@ -1,22 +1,42 @@
 package edu.kit.pse.ass.facility.dao;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import javax.inject.Inject;
+
+import org.springframework.orm.jpa.JpaTemplate;
 
 import edu.kit.pse.ass.entity.Facility;
 import edu.kit.pse.ass.entity.Property;
 
 public class FacilityDAOImpl implements FacilityDAO {
 
+	@Inject
+	private JpaTemplate jpaTemplate;
+
+	@Override
 	public Facility getFacility(String facilityID) {
-		// TODO Auto-generated method stub
-		return null;
+		return jpaTemplate.find(Facility.class, facilityID);
 	}
 
+	@SuppressWarnings("unchecked")
+	@Override
 	public Collection<Facility> getFacilities(Collection<Property> properties) {
-		// TODO Auto-generated method stub
-		return null;
+		String query = "from t_facility f where 1=1 ";
+
+		HashMap<String, Property> props = new HashMap<String, Property>();
+		Iterator<Property> propiter = properties.iterator();
+		for (int i = 0; i < properties.size(); i++) {
+			query += " and :prop" + i + " in elements(f.properties) ";
+			props.put("prop" + i, propiter.next());
+		}
+
+		return jpaTemplate.findByNamedParams(query, props);
 	}
 
+	@Override
 	public Collection<Property> getAvailablePropertiesOf(
 			Class<? extends Facility> facilityClass) {
 		// TODO Auto-generated method stub
