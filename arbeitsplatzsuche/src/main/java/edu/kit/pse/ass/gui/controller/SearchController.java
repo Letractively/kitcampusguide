c7cf34fb9697ecc70132994d9125c68ed0aecfa2
oliver.schneider2 <oliver.schneider2@student.kit.edu>
2011-12-30 21:13:39 +0000

    -Send SearchFormModel data to listSearchResults

--- arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/gui/controller/SearchController.java
+++ arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/gui/controller/SearchController.java
@@ -16,6 +16,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import edu.kit.pse.ass.booking.management.FreeFacilityResult;
@@ -30,23 +31,24 @@ public class SearchController extends MainController {
 
 	@RequestMapping(value = "search/simple.html")
 	public String setUpSimpleSearch(Model model) {
-		prefillSearchForm(model);
+		prefillSearchForm(new SearchFormModel(), model);
 		return "search/simple";
 	}
 
 	@RequestMapping(value = "search/advanced.html")
-	public String setUpAdvancedSearch(Model model) {
-		prefillSearchForm(model);
+	public String setUpAdvancedSearch(@ModelAttribute SearchFormModel sfm,
+			Model model) {
+		prefillSearchForm(sfm, model);
 		return "search/advanced";
 	}
 
-	private void prefillSearchForm(Model model) {
-		model.addAttribute("searchFormModel", new SearchFormModel());
+	private void prefillSearchForm(SearchFormModel sfm, Model model) {
+		model.addAttribute("searchFormModel", sfm);
 	}
 
 	@RequestMapping(value = "search/results")
-	public void listSearchResults(HttpServletRequest request,
-			HttpServletResponse response) {
+	public void listSearchResults(@ModelAttribute SearchFormModel sfm,
+			HttpServletRequest request, HttpServletResponse response) {
 
 		// Get parameters for DataTable
 		DataTableParamModel parameters = DataTablesParamUtility
