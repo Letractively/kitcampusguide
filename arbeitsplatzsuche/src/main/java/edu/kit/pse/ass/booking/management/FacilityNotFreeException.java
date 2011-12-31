5283c811c1d953a5d5848d58bbfa9f689b926fec
andreas.bosch <andreas.bosch@student.kit.edu>
2011-12-31 13:44:20 +0000

    removed problematic unused constructor in FacilityNotFreeException

--- arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/booking/management/FacilityNotFreeException.java
+++ arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/booking/management/FacilityNotFreeException.java
@@ -50,21 +50,4 @@ public class FacilityNotFreeException extends Exception {
 		super(message, cause);
 	}
 
-	/**
-	 * Instantiates a new facility not free exception.
-	 * 
-	 * @param message
-	 *            the message
-	 * @param cause
-	 *            the cause
-	 * @param enableSuppression
-	 *            the enable suppression
-	 * @param writableStackTrace
-	 *            the writable stack trace
-	 */
-	public FacilityNotFreeException(String message, Throwable cause,
-			boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-	}
-
 }
