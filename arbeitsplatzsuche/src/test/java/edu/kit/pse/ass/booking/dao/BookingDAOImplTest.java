b118246ada3ee442651745fa493a796114c7d2b9
lennart.gilbert <lennart.gilbert@student.kit.edu>
2011-12-31 01:28:06 +0000

    - Expanded BookingDAOImplTest - Expanded BookingManagementImplTest both for null parameter check and minor changes

--- arbeitsplatzsuche/src/test/java/edu/kit/pse/ass/booking/dao/BookingDAOImplTest.java
+++ arbeitsplatzsuche/src/test/java/edu/kit/pse/ass/booking/dao/BookingDAOImplTest.java
@@ -1,11 +1,11 @@
 package edu.kit.pse.ass.booking.dao;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertTrue;
 
-import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -29,14 +29,13 @@ public class BookingDAOImplTest {
 	 * Setup a BookingDAO with a dummy reservation
 	 * */
 	BookingDAO bm = new BookingDAOImpl();
-	Date start = new GregorianCalendar(2012, 11, 30, 9, 0).getTime();
-	Date end = new GregorianCalendar(2012, 11, 30, 10, 0).getTime();
+	Date start = new GregorianCalendar(2012, 0, 2, 9, 0).getTime();
+	Date end = new GregorianCalendar(2012, 0, 2, 10, 0).getTime();
 	Reservation testReservation = new Reservation(start, end, USERID);
 	Collection<Reservation> testReservationCol = null;
 
 	@Before
 	public void setUp() throws Exception {
-		// TODO might add more
 		testReservation.addBookedFacilityId(FACILITYID);
 		testReservation.setId(PERSISTED_RESERVATIONID);
 		testReservationCol.add(testReservation);
@@ -46,83 +45,111 @@ public class BookingDAOImplTest {
 
 	@Test
 	public void testGetReservationsOfUser() {
-		Date from = new GregorianCalendar(2012, 11, 30).getTime();
-		Date to = new GregorianCalendar(2012, 11, 31).getTime();
+		Date from = new GregorianCalendar(2012, 0, 2).getTime();
+		Date to = new GregorianCalendar(2012, 0, 3).getTime();
 		try {
-			assertNotNull("accepted wrong parameters. ",
+			assertNull("Accepted wrong parameters.",
 					bm.getReservationsOfUser(null, from, to));
-		} catch (Exception e) {
+			assertNull("Accepted wrong parameters.",
+					bm.getReservationsOfFacility(USERID, null, to));
+			assertNull("Accepted wrong parameters.",
+					bm.getReservationsOfFacility(USERID, from, null));
 
+			assertTrue(bm.getReservationsOfUser(USERID, start, end)
+					.containsAll(testReservationCol));
+			assertTrue(bm.getReservationsOfUser(USERID, from, to).containsAll(
+					testReservationCol));
+			assertTrue(bm.getReservationsOfUser(USERID, start, to).containsAll(
+					testReservationCol));
+			assertFalse(bm.getReservationsOfUser(USERID, end, to).containsAll(
+					testReservationCol));
+		} catch (Exception e) {
+			System.out.println("Error: " + e);
 		}
-		assertEquals(testReservationCol,
-				bm.getReservationsOfUser(USERID, from, to));
 	}
 
 	@Test
 	public void testGetReservationsOfFacility() {
-		Date from = new GregorianCalendar(2012, 11, 30).getTime();
-		Date to = new GregorianCalendar(2012, 11, 31).getTime();
+		Date from = new GregorianCalendar(2012, 0, 2).getTime();
+		Date to = new GregorianCalendar(2012, 0, 3).getTime();
 		try {
-			assertNotNull("accepted wrong parameters. ",
+			assertNull("Accepted wrong parameters.",
 					bm.getReservationsOfFacility(null, from, to));
-		} catch (Exception e) {
+			assertNull("Accepted wrong parameters.",
+					bm.getReservationsOfFacility(FACILITYID, null, to));
+			assertNull("Accepted wrong parameters.",
+					bm.getReservationsOfFacility(FACILITYID, from, null));
 
+			assertTrue(bm.getReservationsOfFacility(FACILITYID, from, to)
+					.containsAll(testReservationCol));
+			assertTrue(bm.getReservationsOfFacility(FACILITYID, start, end)
+					.containsAll(testReservationCol));
+			assertTrue(bm.getReservationsOfFacility(FACILITYID, start, to)
+					.containsAll(testReservationCol));
+			assertFalse(bm.getReservationsOfFacility(FACILITYID, end, to)
+					.containsAll(testReservationCol));
+		} catch (Exception e) {
+			System.out.println("Error: " + e);
 		}
-		assertEquals(testReservationCol,
-				bm.getReservationsOfFacility(FACILITYID, from, to));
 	}
 
 	@Test
 	public void testGetReservation() {
+		Reservation resv = null;
 		try {
-			assertNotNull("accepted wrong parameters. ",
-					bm.getReservation(null));
-		} catch (Exception e) {
+			assertNull("Accepted wrong parameters.", bm.getReservation(null));
 
+			resv = bm.getReservation(PERSISTED_RESERVATIONID);
+			assertNotNull("Didn't return reservation", resv);
+		} catch (Exception e) {
+			System.out.println("Error: " + e);
 		}
-		assertEquals(PERSISTED_RESERVATIONID,
-				bm.getReservation(PERSISTED_RESERVATIONID).getId());
+		assertEquals(PERSISTED_RESERVATIONID, resv.getId());
+		assertTrue(resv.getBookedFacilityIds().contains(FACILITYID));
+		assertEquals(start, resv.getStartTime());
+		assertEquals(end, resv.getEndTime());
 	}
 
 	@Test
 	public void testInsertReservation() {
 		try {
-			assertNotNull("accepted wrong parameters. ",
-					bm.insertReservation(null));
-		} catch (Exception e) {
+			assertNull("Accepted wrong parameters.", bm.insertReservation(null));
 
+			assertEquals(bm.insertReservation(testReservation),
+					PERSISTED_RESERVATIONID);
+			assertEquals(bm.getReservation(PERSISTED_RESERVATIONID),
+					PERSISTED_RESERVATIONID);
+		} catch (Exception e) {
+			System.out.println("Error: " + e);
 		}
-		assertEquals(bm.insertReservation(testReservation),
-				PERSISTED_RESERVATIONID);
 	}
 
 	@Test
 	public void testUpdateReservation() {
-		Calendar end = new GregorianCalendar();
-		Date newEnd = new GregorianCalendar(2012, 11, 30, 11, 0).getTime();
-		end.setTime(newEnd);
-		testReservation.setEndTime(end.getTime());
+		Date newEnd = new GregorianCalendar(2012, 0, 2, 11, 0).getTime();
+		testReservation.setEndTime(newEnd);
 		try {
 			bm.updateReservation(null);
+			bm.updateReservation(testReservation);
 		} catch (Exception e) {
-			fail("Error: " + e);
+			System.out.println("Error: " + e);
 		}
-		bm.updateReservation(testReservation);
-		assertEquals(newEnd, bm.getReservation(PERSISTED_RESERVATIONID)
-				.getEndTime());
+		Reservation resv = bm.getReservation(PERSISTED_RESERVATIONID);
+		assertEquals(newEnd, resv.getEndTime());
+		assertEquals(start, resv.getStartTime());
+		assertTrue(resv.getBookedFacilityIds().contains(FACILITYID));
 	}
 
 	@Test
 	public void testDeleteReservation() {
 		try {
 			bm.deleteReservation(null);
-
+			bm.deleteReservation(PERSISTED_RESERVATIONID);
 		} catch (Exception e) {
-			fail("Error: " + e);
+			System.out.println("Error: " + e);
 		}
-		bm.deleteReservation(PERSISTED_RESERVATIONID);
 		/*
-		 * TODO wait for return of method getReservation if id not used
+		 * TODO wait for return of getReservation if id not used
 		 */
 		assertNull(bm.getReservation(PERSISTED_RESERVATIONID));
 	}
