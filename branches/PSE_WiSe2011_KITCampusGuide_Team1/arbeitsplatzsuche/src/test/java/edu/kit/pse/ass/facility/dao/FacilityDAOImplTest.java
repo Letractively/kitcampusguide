9dd1331c9a650399a851463c6e1138fd9c461bd2
lennart.gilbert@student.kit.edu <lennart.gilbert@student.kit.edu@02b7392b-9286-25d4-34bb-9ed0ff935e76>
2012-01-01 17:05:53 +0000

    Implemented the tests in FacilityDAOImplTest

--- branches/PSE_WiSe2011_KITCampusGuide_Team1/arbeitsplatzsuche/src/test/java/edu/kit/pse/ass/facility/dao/FacilityDAOImplTest.java
+++ branches/PSE_WiSe2011_KITCampusGuide_Team1/arbeitsplatzsuche/src/test/java/edu/kit/pse/ass/facility/dao/FacilityDAOImplTest.java
@@ -1,34 +1,110 @@
 package edu.kit.pse.ass.facility.dao;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
 import java.util.ArrayList;
 import java.util.Collection;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import edu.kit.pse.ass.entity.Facility;
 import edu.kit.pse.ass.entity.Property;
 import edu.kit.pse.ass.entity.Room;
+import edu.kit.pse.ass.entity.Workplace;
 
+/**
+ * @author Lennart
+ * 
+ */
 public class FacilityDAOImplTest {
-	private static final String FACILITYID = "#SOME_FACILITY_ID#";
+	private static final String FACILITYID = "ID###1";
+	FacilityDAO dao = new FacilityDAOImpl();
+	Collection<Property> props = null;
+	Collection<Facility> places = null;
+	Collection<Facility> facs = null;
+
+	@Before
+	public void setUp() {
+		Property prop1 = new Property("WLAN");
+		Facility place1 = new Workplace();
+		place1.setName("place1");
+		Facility place2 = new Workplace();
+		place2.setName("place2");
+		Facility place3 = new Workplace();
+		place3.setName("place3");
+		Facility place4 = new Workplace();
+		place4.setName("place4");
+		Facility facil1 = new Room();
+		Facility facil2 = new Room();
+		Facility facil3 = new Room();
+		Facility facil4 = new Room();
+		facil1.setName("ID###1");
+		facil2.setName("ID###2");
+		facil3.setName("ID###3");
+		facil4.setName("ID###4");
+
+		dao.facilityFillWithDummies();
+		props.add(prop1);
+		places.add(place1);
+		places.add(place2);
+		places.add(place3);
+		places.add(place4);
+		facs.add(facil1);
+		facs.add(facil2);
+		facs.add(facil3);
+		facs.add(facil4);
+	}
 
 	@Test
 	public void testGetFacility() {
-		FacilityDAO dao = new FacilityDAOImpl();
-		dao.getFacility(FACILITYID);
+		Facility result = null;
+		try {
+			assertNull(dao.getFacility(null));
+			result = dao.getFacility(FACILITYID);
+		} catch (Exception e) {
+			System.out.println("Error: " + e);
+		}
+		assertNotNull(result);
+		assertEquals(FACILITYID, result.getId());
+		assertTrue(result.getContainedFacilities().containsAll(places));
+		assertTrue(result.getProperties().containsAll(props));
 	}
 
 	@Test
 	public void testGetFacilities() {
-		FacilityDAO dao = new FacilityDAOImpl();
-		Collection<Property> properties = new ArrayList<Property>();
-		properties.add(new Property("WLAN"));
-		dao.getFacilities(properties);
+		Collection<Facility> result = null;
+		try {
+			assertNull(dao.getFacilities(null));
+			result = dao.getFacilities(props);
+		} catch (Exception e) {
+			System.out.println("Error: " + e);
+		}
+		assertNotNull(result);
+		assertTrue(result.containsAll(facs));
+		for (Facility fac : result) {
+			assertTrue(fac.getProperties().containsAll(props));
+		}
+
 	}
 
 	@Test
 	public void testGetAvailablePropertiesOf() {
-		FacilityDAO dao = new FacilityDAOImpl();
-		dao.getAvailablePropertiesOf(Room.class);
+		Collection<Property> result = null;
+		Collection<Property> expected = new ArrayList<Property>();
+		expected.add(new Property("WLAN"));
+		expected.add(new Property("Steckdose"));
+		try {
+			assertNull(dao.getAvailablePropertiesOf(null));
+			result = dao.getAvailablePropertiesOf(Room.class);
+		} catch (Exception e) {
+			System.out.println("Error: " + e);
+		}
+		assertNotNull(result);
+		assertTrue(result.containsAll(expected));
+		assertEquals(result.size(), expected.size());
 	}
-
 }
