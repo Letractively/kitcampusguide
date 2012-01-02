0f527dbf402f0583719d26d5a751bb65de2a3dde
andreas.bosch <andreas.bosch@student.kit.edu>
2012-01-02 22:02:57 +0000

    implemented UserDAOImpl

--- arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/user/dao/UserDAOImpl.java
+++ arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/user/dao/UserDAOImpl.java
@@ -1,25 +1,40 @@
 package edu.kit.pse.ass.user.dao;
 
+import javax.inject.Inject;
+
+import org.springframework.orm.jpa.JpaTemplate;
+import org.springframework.transaction.annotation.Transactional;
+
 import edu.kit.pse.ass.entity.User;
 
 public class UserDAOImpl implements UserDAO {
 
+	/** The jpa template. */
+	@Inject
+	private JpaTemplate jpaTemplate;
+
 	@Override
 	public User insertUser(String userID, String passwordHash) {
-		// TODO Auto-generated method stub
-		return null;
+		User u = new User();
+		u.setEmail(userID);
+		u.setId(userID);
+		u.setPassword(passwordHash);
+		jpaTemplate.persist(u);
+		return u;
 	}
 
 	@Override
+	@Transactional
 	public void deleteUser(String userID) {
-		// TODO Auto-generated method stub
-
+		User u = jpaTemplate.find(User.class, userID);
+		if (u != null) {
+			jpaTemplate.remove(u);
+		}
 	}
 
 	@Override
 	public User getUser(String userID) {
-		// TODO Auto-generated method stub
-		return null;
+		return jpaTemplate.find(User.class, userID);
 	}
 
 	@Override
