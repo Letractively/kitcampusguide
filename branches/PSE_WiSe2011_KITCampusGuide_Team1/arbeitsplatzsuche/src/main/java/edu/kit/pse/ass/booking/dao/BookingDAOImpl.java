3fb01215accb30a42d12247c64b29e0fdd449f8b
lennart.gilbert@student.kit.edu <lennart.gilbert@student.kit.edu@02b7392b-9286-25d4-34bb-9ed0ff935e76>
2012-01-02 16:42:54 +0000

    Implemented bookingFillWithDummies(), made it easy to expand the entries

--- branches/PSE_WiSe2011_KITCampusGuide_Team1/arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/booking/dao/BookingDAOImpl.java
+++ branches/PSE_WiSe2011_KITCampusGuide_Team1/arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/booking/dao/BookingDAOImpl.java
@@ -1,11 +1,23 @@
 package edu.kit.pse.ass.booking.dao;
 
+import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.orm.jpa.JpaTemplate;
 
 import edu.kit.pse.ass.entity.Reservation;
 
 public class BookingDAOImpl implements BookingDAO {
+
+	/** The jpa template. */
+	@Inject
+	private JpaTemplate jpaTemplate;
 
 	@Override
 	public Collection<Reservation> getReservationsOfUser(String userID,
@@ -47,8 +59,43 @@ public class BookingDAOImpl implements BookingDAO {
 
 	@Override
 	public void bookingFillWithDummies() {
-		// TODO Auto-generated method stub
+		// TODO
+		// the available facilities
+		List<String> facilityIDs = Arrays.asList("ID##01", "ID##02", "ID##03",
+				"ID##04", "ID##05", "ID##06");
+		// the reservations IDs
+		List<String> resvIDs = Arrays.asList("RID0001", "RID0002", "RID0003",
+				"RID0004", "RID0005", "RID0006", "RID0007");
+		// the userIDs
+		List<String> userIDs = Arrays.asList("uaaaa@student.kit.edu",
+				"uaaab@student.kit.edu", "uaaac@student.kit.edu",
+				"uaaad@student.kit.edu", "uaaae@student.kit.edu",
+				"uaaaf@student.kit.edu");
+		// the start dates
+		List<GregorianCalendar> start = Arrays.asList(new GregorianCalendar(
+				2012, 1, 1, 9, 0), new GregorianCalendar(2012, 1, 1, 12, 0),
+				new GregorianCalendar(2012, 1, 1, 15, 0));
+		// the end dates, all of them after the latest start
+		List<GregorianCalendar> end = Arrays.asList(new GregorianCalendar(2012,
+				1, 1, 11, 0), new GregorianCalendar(2012, 1, 1, 14, 0),
+				new GregorianCalendar(2012, 1, 1, 17, 0));
 
+		// create reservations and persist them
+		int i = 0;
+		int k = resvIDs.size();
+		for (String resvID : resvIDs) {
+			// makes the lists size independent
+			i %= k;
+			Reservation resvTmp = new Reservation(start.get(i).getTime(), end
+					.get(i).getTime(), userIDs.get(i));
+			resvTmp.addBookedFacilityId(facilityIDs.get(i));
+			resvTmp.setId(resvID);
+			jpaTemplate.persist(resvTmp);
+			// adds a day to each star and end, thus preventing double
+			// reservations
+			start.get(i).add(Calendar.DATE, 1);
+			end.get(i).add(Calendar.DATE, 1);
+			i++;
+		}
 	}
-
 }
