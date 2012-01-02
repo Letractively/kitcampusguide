4223836dfc20ea65382ef7e94b256465488ca571
andreas.bosch@student.kit.edu <andreas.bosch@student.kit.edu@02b7392b-9286-25d4-34bb-9ed0ff935e76>
2012-01-02 15:13:51 +0000

    first implementation of findFreeFacilities

--- branches/PSE_WiSe2011_KITCampusGuide_Team1/arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/booking/management/BookingManagementImpl.java
+++ branches/PSE_WiSe2011_KITCampusGuide_Team1/arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/booking/management/BookingManagementImpl.java
@@ -1,12 +1,27 @@
 package edu.kit.pse.ass.booking.management;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
+import java.util.Iterator;
 
+import javax.inject.Inject;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.kit.pse.ass.booking.dao.BookingDAO;
+import edu.kit.pse.ass.entity.Facility;
 import edu.kit.pse.ass.entity.Reservation;
+import edu.kit.pse.ass.facility.management.FacilityManagement;
 import edu.kit.pse.ass.facility.management.FacilityQuery;
 
 public class BookingManagementImpl implements BookingManagement {
+
+	@Inject
+	BookingDAO bookingDAO;
+
+	@Inject
+	FacilityManagement facilityManagement;
 
 	@Override
 	public String book(String userID, Collection<String> facilityIDs,
@@ -57,18 +72,63 @@ public class BookingManagementImpl implements BookingManagement {
 	@Override
 	public Collection<FreeFacilityResult> findFreeFacilites(
 			FacilityQuery query, Date start, Date end, boolean fullyAvailable) {
-		/*
-		 * TODO either a database of facilities must be specified to use always
-		 * or it must be given to the method
-		 */
-		return null;
+		// the concrete instance of FacilityManagement is specified via
+		// Dependency Injection
+		// fetch matching facilites
+		Collection<? extends Facility> facilityCollection = facilityManagement
+				.findMatchingFacilities(query);
+		// create output array
+		ArrayList<FreeFacilityResult> facilityResult = new ArrayList<FreeFacilityResult>();
+		Date startDate = (Date) start.clone();
+		Date endDate = (Date) end.clone();
+
+		// find free matching facilities
+		// if less than 5 are found, shift the reservation time forward up to
+		// 2 hours.
+		int addQuarterHour = 0;
+		while (!facilityCollection.isEmpty() && addQuarterHour < 9
+				&& facilityResult.size() < 5) {
+			Iterator<? extends Facility> facilityIterator = facilityCollection
+					.iterator();
+			// collect all free facilities
+			while (facilityIterator.hasNext()) {
+				Facility facility = facilityIterator.next();
+				if (isFacilityFree(facility.getId(), startDate, endDate)) {
+					// add the found facility
+					facilityResult.add(new FreeFacilityResult(facility,
+							(Date) startDate.clone()));
+					// remove from queue
+					facilityCollection.remove(facility);
+				}
+			}
+			// shift reservation time 15 minutes in the future.
+			++addQuarterHour;
+			startDate.setTime(startDate.getTime() + 15 * 60000);
+			endDate.setTime(endDate.getTime() + 15 * 60000);
+		}
+		return facilityResult;
 	}
 
 	@Override
+	@Transactional
 	public boolean isFacilityFree(String facilityID, Date startDate,
 			Date endDate) {
-		// TODO Auto-generated method stub
-		return false;
+		Collection<Reservation> reservations = bookingDAO
+				.getReservationsOfFacility(facilityID, startDate, endDate);
+		if (reservations != null && reservations.size() > 0) {
+			return false;
+		}
+		Facility facility = facilityManagement.getFacility(facilityID);
+		Facility parent = facility.getParentFacility();
+		if (parent != null
+				&& !isFacilityFree(parent.getId(), startDate, endDate)) {
+			return false;
+		}
+		for (Facility containedFacility : facility.getContainedFacilities()) {
+			if (!isFacilityFree(containedFacility.getId(), startDate, endDate)) {
+				return false;
+			}
+		}
+		return true;
 	}
-
 }
