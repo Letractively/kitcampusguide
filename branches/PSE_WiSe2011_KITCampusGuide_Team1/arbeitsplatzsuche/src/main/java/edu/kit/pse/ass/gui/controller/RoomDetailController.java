1ce74cde8f9e84d00b05322a0623dfc4dad14b55
oliver.schneider2@student.kit.edu <oliver.schneider2@student.kit.edu@02b7392b-9286-25d4-34bb-9ed0ff935e76>
2012-01-04 10:38:54 +0000

    -GUI: Mapping for RoomDetails -GUI: JSP for RoomDetails

--- branches/PSE_WiSe2011_KITCampusGuide_Team1/arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/gui/controller/RoomDetailController.java
+++ branches/PSE_WiSe2011_KITCampusGuide_Team1/arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/gui/controller/RoomDetailController.java
@@ -1,14 +1,33 @@
 package edu.kit.pse.ass.gui.controller;
 
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import edu.kit.pse.ass.entity.Facility;
+import edu.kit.pse.ass.entity.Room;
+import edu.kit.pse.ass.facility.management.FacilityManagement;
 
 @Controller
 public class RoomDetailController extends MainController {
 
-	@RequestMapping(value = "room/details")
-	public String setUpRoomDetails(Model model) {
+	@Inject
+	FacilityManagement facilityManagement;
+
+	@RequestMapping(value = "room/{roomId}/details.html")
+	public String setUpRoomDetails(@PathVariable("roomId") String roomId,
+			Model model) {
+		Facility f = facilityManagement.getFacility(roomId);
+
+		if (!(f instanceof Room)) {
+			// TODO error!
+		}
+
+		Room room = (Room) f;
+		model.addAttribute("room", room);
 		return "room/details";
 	}
 
