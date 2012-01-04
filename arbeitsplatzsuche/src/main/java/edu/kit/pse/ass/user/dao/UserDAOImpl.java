07e37709d6871eb90a05f1c6e3af9666a661ff26
andreas.bosch <andreas.bosch@student.kit.edu>
2012-01-04 19:14:50 +0000

    - minimal implementation of Spring Security using Dao-Acccess (filter for web access not enabled) - multiple roles are allowed for a single user

--- arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/user/dao/UserDAOImpl.java
+++ arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/user/dao/UserDAOImpl.java
@@ -1,16 +1,21 @@
 package edu.kit.pse.ass.user.dao;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 import javax.inject.Inject;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.orm.jpa.JpaTemplate;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.transaction.annotation.Transactional;
 
 import edu.kit.pse.ass.entity.User;
 
-public class UserDAOImpl implements UserDAO {
+public class UserDAOImpl implements UserDAO, UserDetailsService {
 
 	/** The jpa template. */
 	@Inject
@@ -41,6 +46,12 @@ public class UserDAOImpl implements UserDAO {
 	}
 
 	@Override
+	public UserDetails loadUserByUsername(String username)
+			throws UsernameNotFoundException, DataAccessException {
+		return jpaTemplate.find(User.class, username);
+	}
+
+	@Override
 	public void userFillWithDummies() {
 		// TODO Auto-generated method stub
 		// 4 letter email part, used as id
@@ -48,12 +59,13 @@ public class UserDAOImpl implements UserDAO {
 				"bbbf", "bbbg", "bbbh", "bbbi", "bbbj", "bbbk", "bbbl", "bbbm");
 		for (int i = 0; i < email.size(); i++) {
 			User u = new User();
-			u.setRole("student");
+			HashSet<String> s = new HashSet<String>();
+			s.add("student");
+			u.setRoles(s);
 			u.setId("u" + email.get(i) + "@student.kit.edu");
 			u.setEmail("u" + email.get(i) + "@student.kit.edu");
 			u.setPassword(email.get(i) + email.get(i));
 			jpaTemplate.persist(u);
 		}
 	}
-
 }
