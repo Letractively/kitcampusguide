bbc2f25dde3a830aaad4bdc6078f4a8b35399bea
andreas.bosch@student.kit.edu <andreas.bosch@student.kit.edu@02b7392b-9286-25d4-34bb-9ed0ff935e76>
2012-01-06 19:26:14 +0000

    removed dummy data in UserDAO

--- branches/PSE_WiSe2011_KITCampusGuide_Team1/arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/user/dao/UserDAOImpl.java
+++ branches/PSE_WiSe2011_KITCampusGuide_Team1/arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/user/dao/UserDAOImpl.java
@@ -1,9 +1,5 @@
 package edu.kit.pse.ass.user.dao;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DataRetrievalFailureException;
@@ -11,7 +7,6 @@ import org.springframework.orm.jpa.JpaTemplate;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.transaction.annotation.Transactional;
 
 import edu.kit.pse.ass.entity.User;
 
@@ -44,19 +39,8 @@ public class UserDAOImpl implements UserDAO, UserDetailsService {
 	}
 
 	@Override
-	@Transactional(readOnly = true)
 	public UserDetails loadUserByUsername(String username)
 			throws UsernameNotFoundException, DataAccessException {
-		// TODO Create testdata at the correct place
-		// TODO This user is ubbbb@student.kit.edu with Password bbbbbbbb
-		User u1 = new User();
-		u1.setEmail("ubbbb@student.kit.edu");
-		u1.setPassword("fb398cc690e15ddba43ee811b6c0d3ec190901ad3df377fec9a1f9004b919a06");
-		HashSet<String> s = new HashSet<String>();
-		s.add("ROLE_STUDENT");
-		u1.setRoles(s);
-		jpaTemplate.persist(u1);
-
 		try {
 			User u = jpaTemplate.find(User.class, username);
 			if (u == null) {
@@ -68,21 +52,4 @@ public class UserDAOImpl implements UserDAO, UserDetailsService {
 		}
 	}
 
-	@Override
-	@Transactional
-	public void userFillWithDummies() {
-		// TODO Auto-generated method stub
-		// 4 letter email part, used as id
-		List<String> email = Arrays.asList("bbbb", "bbbc", "bbbd", "bbbe",
-				"bbbf", "bbbg", "bbbh", "bbbi", "bbbj", "bbbk", "bbbl", "bbbm");
-		for (int i = 0; i < email.size(); i++) {
-			User u = new User();
-			HashSet<String> s = new HashSet<String>();
-			s.add("student");
-			u.setRoles(s);
-			u.setEmail("u" + email.get(i) + "@student.kit.edu");
-			u.setPassword(email.get(i) + email.get(i));
-			jpaTemplate.persist(u);
-		}
-	}
 }
