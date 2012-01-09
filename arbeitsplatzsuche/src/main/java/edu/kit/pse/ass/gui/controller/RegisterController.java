bc9dedc8daaefa3e295ca02f66addbb82ef46921
ufddz <ufddz@student.kit.edu>
2012-01-09 11:05:38 +0000

    Created register view

--- arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/gui/controller/RegisterController.java
+++ arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/gui/controller/RegisterController.java
@@ -2,6 +2,10 @@ package edu.kit.pse.ass.gui.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import edu.kit.pse.ass.entity.User;
 
 // TODO: Auto-generated Javadoc
 /**
@@ -12,18 +16,22 @@ public class RegisterController extends MainController {
 
 	/**
 	 * Sets the up register form.
-	 *
-	 * @param model the model
+	 * 
+	 * @param model
+	 *            the model
 	 * @return the string
 	 */
+	@RequestMapping(value = "register.html", method = RequestMethod.GET)
 	public String setUpRegisterForm(Model model) {
-		return "";
+		model.addAttribute("registerUser", new User());
+		return "register/register";
 	}
 
 	/**
 	 * Register user.
-	 *
-	 * @param model the model
+	 * 
+	 * @param model
+	 *            the model
 	 * @return the string
 	 */
 	public String registerUser(Model model) {
