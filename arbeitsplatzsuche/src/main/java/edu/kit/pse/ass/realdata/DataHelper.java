d7edcbb4f3c9005be0ce34a1017433d1102e8294
lennart.gilbert <lennart.gilbert@student.kit.edu>
2012-01-31 23:48:33 +0000

    -DataHelper: added javaDoc -UserManagementImplTest: started implementation of tests

--- arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/realdata/DataHelper.java
+++ arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/realdata/DataHelper.java
@@ -3,7 +3,6 @@
  */
 package edu.kit.pse.ass.realdata;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 import java.util.HashSet;
@@ -32,6 +31,17 @@ public class DataHelper {
 	@Autowired
 	private PasswordEncoder passwordEncoder;
 
+	/**
+	 * Add a building to the db with following attributes:
+	 * 
+	 * @param name
+	 *            - the name of the building (e.g. "Informatik Fakultät")
+	 * @param number
+	 *            - the number of the building (e.g. 50.34)
+	 * @param properties
+	 *            - a Collection of properties the building owns
+	 * @return the persisted building
+	 */
 	public Building createPersistedBuilding(String name, String number, Collection<Property> properties) {
 		Building t = new Building();
 		t.setName(name);
@@ -43,6 +53,20 @@ public class DataHelper {
 		return t;
 	}
 
+	/**
+	 * Add a room to the db with following attributes:
+	 * 
+	 * @param name
+	 *            - the name of the room (e.g. "Seminarraum")
+	 * @param number
+	 *            - the number of the room (e.g. "-118")
+	 * 
+	 * @param level
+	 *            - the level of the room (e.g. "-1")
+	 * @param properties
+	 *            - a Collection of properties the room owns
+	 * @return the persisted room
+	 */
 	public Room createPersistedRoom(String name, String number, int level, Collection<Property> properties) {
 		Room t = new Room();
 		t.setName(name);
@@ -55,6 +79,15 @@ public class DataHelper {
 		return t;
 	}
 
+	/**
+	 * Add a workplace to the db with following attributes:
+	 * 
+	 * @param name
+	 *            - the name of the workplace (e.g. "1.5", desk 1 place 5)
+	 * @param properties
+	 *            - a Collection of properties the workplace owns
+	 * @return the persisted room
+	 */
 	public Workplace createPersistedWorkplace(String name, Collection<Property> properties) {
 		Workplace t = new Workplace();
 		t.setName(name);
@@ -65,6 +98,17 @@ public class DataHelper {
 		return t;
 	}
 
+	/**
+	 * Add a user to the db with following attributes:
+	 * 
+	 * @param email
+	 *            - the email of the user (must match: "u[a-z]{4}@student.kit.edu")
+	 * @param password
+	 *            - the password for the account (must be 8 characters long)
+	 * @param roles
+	 *            - the roles the users holds
+	 * @return the persisted user
+	 */
 	public User createPersistedUser(String email, String password, HashSet<String> roles) {
 		User u = new User();
 		u.setRoles(roles);
@@ -74,6 +118,19 @@ public class DataHelper {
 		return u;
 	}
 
+	/**
+	 * Add a reservation to the db with following attributes:
+	 * 
+	 * @param userID
+	 *            - the id of the reserving user
+	 * @param facilityIDs
+	 *            - the ids of the reserved facilities
+	 * @param start
+	 *            - start date of the reservation
+	 * @param end
+	 *            - end date of the reservation
+	 * @return the persisted reservation
+	 */
 	public Reservation createPersistedReservation(String userID, Collection<String> facilityIDs, Date start,
 			Date end) {
 		Reservation r = new Reservation();
