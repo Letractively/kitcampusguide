53695a3eef8fb502b6ba530faa008575e2201a09
oliver.schneider2@student.kit.edu <oliver.schneider2@student.kit.edu@02b7392b-9286-25d4-34bb-9ed0ff935e76>
2012-02-02 08:29:58 +0000

    -Check that data is inserted only once in db

--- branches/PSE_WiSe2011_KITCampusGuide_Team1/arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/realdata/RealData.java
+++ branches/PSE_WiSe2011_KITCampusGuide_Team1/arbeitsplatzsuche/src/main/java/edu/kit/pse/ass/realdata/RealData.java
@@ -24,21 +24,14 @@ public class RealData {
 	@Autowired
 	private DataHelper dataHelper;
 
-	private boolean userInseted;
-
-	/**
-	 * empty constructor
-	 */
-	/*
-	 * public RealData() { loadAllData(); }
-	 */
+	private static boolean userInseted = false;
+	private static boolean facilitiesInseted = false;
 
 	/**
 	 * Load all data.
 	 */
 	@Transactional
 	public void loadAllData() {
-		userInseted = false;
 		realFacilities();
 		realUsers();
 	}
@@ -70,111 +63,116 @@ public class RealData {
 	 */
 	@Transactional
 	public void realFacilities() {
-		ArrayList<Property> emptyList = new ArrayList<Property>();
-		// 01 building - Fakultät für Informatik
-		ArrayList<Property> buildingProperties = new ArrayList<Property>(Arrays.asList(new Property("WLAN"),
-				new Property("Barrierefrei")));
-		Building building1 = dataHelper.createPersistedBuilding("Fakultät für Informatik", "50.34",
-				buildingProperties);
-		// 01 rooms
-		ArrayList<Property> roomPropertiesSR = new ArrayList<Property>(Arrays.asList(new Property("Steckdose"),
-				new Property("Beamer"), new Property("Tafel"), new Property("Overhead Projektor")));
-		ArrayList<Property> roomProperties = new ArrayList<Property>(Arrays.asList(new Property("Steckdose"),
-				new Property("LAN"), new Property("Whiteboard")));
-		ArrayList<Property> atisWorkplaces = new ArrayList<Property>(Arrays.asList(new Property("Steckdose"),
-				new Property("LAN"), new Property("PC")));
-		Room room106 = dataHelper.createPersistedRoom("Seminarraum", "-106", -1, roomPropertiesSR);
-		Room room107 = dataHelper.createPersistedRoom("Seminarraum", "-107", -1, roomPropertiesSR);
-		Room room108 = dataHelper.createPersistedRoom("Seminarraum", "-108", -1, roomPropertiesSR);
-		Room room109 = dataHelper.createPersistedRoom("Seminarraum", "-109", -1, roomPropertiesSR);
-		Room room118 = dataHelper.createPersistedRoom("Seminarraum", "-118", -1, roomPropertiesSR);
-		Room room119 = dataHelper.createPersistedRoom("Seminarraum", "-119", -1, roomPropertiesSR);
-		Room room120 = dataHelper.createPersistedRoom("Seminarraum", "-120", -1, roomPropertiesSR);
-		Room room130 = dataHelper.createPersistedRoom("Arbeitsraum", "-130", -1, roomProperties);
-		Room room131 = dataHelper.createPersistedRoom("Arbeitsraum", "-131", -1, roomProperties);
-		Room room132 = dataHelper.createPersistedRoom("Arbeitsraum", "-132", -1, roomProperties);
-		Room room140 = dataHelper.createPersistedRoom("ATIS-Pool 1", "-140", -1, new ArrayList<Property>());
-		Room room141 = dataHelper.createPersistedRoom("ATIS-Pool 2", "-141", -1, new ArrayList<Property>());
-		Collection<Room> rooms50341 = new ArrayList<Room>(Arrays.asList(room106, room107, room108, room109,
-				room118, room119, room120, room130, room131, room132));
-		Collection<Room> rooms50342 = new ArrayList<Room>(Arrays.asList(room140, room141));
+		if (!facilitiesInseted) {
+			ArrayList<Property> emptyList = new ArrayList<Property>();
+			// 01 building - Fakultät für Informatik
+			ArrayList<Property> buildingProperties = new ArrayList<Property>(Arrays.asList(new Property("WLAN"),
+					new Property("Barrierefrei")));
+			Building building1 = dataHelper.createPersistedBuilding("Fakultät für Informatik", "50.34",
+					buildingProperties);
+			// 01 rooms
+			ArrayList<Property> roomPropertiesSR = new ArrayList<Property>(Arrays.asList(
+					new Property("Steckdose"), new Property("Beamer"), new Property("Tafel"), new Property(
+							"Overhead Projektor")));
+			ArrayList<Property> roomProperties = new ArrayList<Property>(Arrays.asList(new Property("Steckdose"),
+					new Property("LAN"), new Property("Whiteboard")));
+			ArrayList<Property> atisWorkplaces = new ArrayList<Property>(Arrays.asList(new Property("Steckdose"),
+					new Property("LAN"), new Property("PC")));
+			Room room106 = dataHelper.createPersistedRoom("Seminarraum", "-106", -1, roomPropertiesSR);
+			Room room107 = dataHelper.createPersistedRoom("Seminarraum", "-107", -1, roomPropertiesSR);
+			Room room108 = dataHelper.createPersistedRoom("Seminarraum", "-108", -1, roomPropertiesSR);
+			Room room109 = dataHelper.createPersistedRoom("Seminarraum", "-109", -1, roomPropertiesSR);
+			Room room118 = dataHelper.createPersistedRoom("Seminarraum", "-118", -1, roomPropertiesSR);
+			Room room119 = dataHelper.createPersistedRoom("Seminarraum", "-119", -1, roomPropertiesSR);
+			Room room120 = dataHelper.createPersistedRoom("Seminarraum", "-120", -1, roomPropertiesSR);
+			Room room130 = dataHelper.createPersistedRoom("Arbeitsraum", "-130", -1, roomProperties);
+			Room room131 = dataHelper.createPersistedRoom("Arbeitsraum", "-131", -1, roomProperties);
+			Room room132 = dataHelper.createPersistedRoom("Arbeitsraum", "-132", -1, roomProperties);
+			Room room140 = dataHelper.createPersistedRoom("ATIS-Pool 1", "-140", -1, new ArrayList<Property>());
+			Room room141 = dataHelper.createPersistedRoom("ATIS-Pool 2", "-141", -1, new ArrayList<Property>());
+			Collection<Room> rooms50341 = new ArrayList<Room>(Arrays.asList(room106, room107, room108, room109,
+					room118, room119, room120, room130, room131, room132));
+			Collection<Room> rooms50342 = new ArrayList<Room>(Arrays.asList(room140, room141));
 
-		// 01 workplaces
-		for (Room tmp : rooms50341) {
-			for (int j = 1; j < 6; j++) {
-				String name = "1." + j;
-				tmp.addContainedFacility(dataHelper.createPersistedWorkplace(name, emptyList));
+			// 01 workplaces
+			for (Room tmp : rooms50341) {
+				for (int j = 1; j < 6; j++) {
+					String name = "1." + j;
+					tmp.addContainedFacility(dataHelper.createPersistedWorkplace(name, emptyList));
+				}
+				building1.addContainedFacility(tmp);
 			}
-			building1.addContainedFacility(tmp);
-		}
 
-		for (Room tmp : rooms50342) {
-			for (int i = 1; i < 11; i++) {
+			for (Room tmp : rooms50342) {
+				for (int i = 1; i < 11; i++) {
+					for (int j = 1; j < 6; j++) {
+						String name = i + "." + j;
+						tmp.addContainedFacility(dataHelper.createPersistedWorkplace(name, atisWorkplaces));
+					}
+				}
+				building1.addContainedFacility(tmp);
+			}
+			// 02 building - Zähringerhaus
+			Building building2 = dataHelper.createPersistedBuilding("Zähringerhaus", "01.85", emptyList);
+			ArrayList<Property> mathbibProperties = new ArrayList<Property>(Arrays.asList(new Property("WLAN"),
+					new Property("Kopierer")));
+			Room roomMathBib = dataHelper
+					.createPersistedRoom("Mathematische Bibliothek", "", 1, mathbibProperties);
+			Room roomVorZ1 = dataHelper.createPersistedRoom("Z1 Vorraum", "", 1,
+					new ArrayList<Property>(Arrays.asList(new Property("WLAN"))));
+			// 02 workplaces
+			for (int i = 1; i < 6; i++) {
 				for (int j = 1; j < 6; j++) {
 					String name = i + "." + j;
-					tmp.addContainedFacility(dataHelper.createPersistedWorkplace(name, atisWorkplaces));
+					roomMathBib.addContainedFacility(dataHelper.createPersistedWorkplace(name, emptyList));
 				}
 			}
-			building1.addContainedFacility(tmp);
-		}
-		// 02 building - Zähringerhaus
-		Building building2 = dataHelper.createPersistedBuilding("Zähringerhaus", "01.85", emptyList);
-		ArrayList<Property> mathbibProperties = new ArrayList<Property>(Arrays.asList(new Property("WLAN"),
-				new Property("Kopierer")));
-		Room roomMathBib = dataHelper.createPersistedRoom("Mathematische Bibliothek", "", 1, mathbibProperties);
-		Room roomVorZ1 = dataHelper.createPersistedRoom("Z1 Vorraum", "", 1,
-				new ArrayList<Property>(Arrays.asList(new Property("WLAN"))));
-		// 02 workplaces
-		for (int i = 1; i < 6; i++) {
-			for (int j = 1; j < 6; j++) {
-				String name = i + "." + j;
-				roomMathBib.addContainedFacility(dataHelper.createPersistedWorkplace(name, emptyList));
-			}
-		}
-		for (int i = 1; i < 4; i++) {
-			for (int j = 1; j < 6; j++) {
-				String name = i + "." + j;
-				roomVorZ1.addContainedFacility(dataHelper.createPersistedWorkplace(name, emptyList));
-			}
-		}
-		building2.addContainedFacility(roomMathBib);
-		building2.addContainedFacility(roomVorZ1);
-
-		// 03 building - Bibliothek
-		Building building3 = dataHelper.createPersistedBuilding("Bibliothek", "30.50", new ArrayList<Property>(
-				Arrays.asList(new Property("WLAN"))));
-		ArrayList<Property> bibWorkplaces = new ArrayList<Property>(Arrays.asList(new Property("Steckdose"),
-				new Property("LAN")));
-		Room roomBib1 = dataHelper.createPersistedRoom("Lesesaal 1", "Altbau", 0, bibWorkplaces);
-		Room roomBib2 = dataHelper.createPersistedRoom("Lesesaal 2", "Neubau", 1, bibWorkplaces);
-		Room roomBib3 = dataHelper.createPersistedRoom("Gruppenraum 1", "Altbau", 1, bibWorkplaces);
-		Room roomBib4 = dataHelper.createPersistedRoom("Gruppenraum 2", "Altbau", 1, bibWorkplaces);
-		Room roomBib5 = dataHelper.createPersistedRoom("Gruppenraum 3", "Altbau", 1, bibWorkplaces);
-		Room roomBib6 = dataHelper.createPersistedRoom("Gruppenraum 4", "Altbau", 1, bibWorkplaces);
-		Room roomBib7 = dataHelper.createPersistedRoom("Gruppenraum 5", "Altbau", 1, bibWorkplaces);
-		Collection<Room> rooms30502 = new ArrayList<Room>(Arrays.asList(roomBib3, roomBib4, roomBib5, roomBib6,
-				roomBib7));
-		// 03 workplaces
-		for (int j = 1; j < 13; j++) {
-			String name = "1." + j;
-			roomBib1.addContainedFacility(dataHelper.createPersistedWorkplace(name, emptyList));
-		}
-		building3.addContainedFacility(roomBib1);
-		for (int i = 1; i < 9; i++) {
-			for (int j = 1; j < 9; j++) {
-				String name = i + "." + j;
-				roomBib2.addContainedFacility(dataHelper.createPersistedWorkplace(name, emptyList));
-			}
-		}
-		building3.addContainedFacility(roomBib2);
-		for (Room tmp : rooms30502) {
-			for (int i = 1; i < 3; i++) {
-				for (int j = 1; j < 7; j++) {
+			for (int i = 1; i < 4; i++) {
+				for (int j = 1; j < 6; j++) {
 					String name = i + "." + j;
-					tmp.addContainedFacility(dataHelper.createPersistedWorkplace(name, atisWorkplaces));
+					roomVorZ1.addContainedFacility(dataHelper.createPersistedWorkplace(name, emptyList));
 				}
 			}
-			building3.addContainedFacility(tmp);
+			building2.addContainedFacility(roomMathBib);
+			building2.addContainedFacility(roomVorZ1);
+
+			// 03 building - Bibliothek
+			Building building3 = dataHelper.createPersistedBuilding("Bibliothek", "30.50",
+					new ArrayList<Property>(Arrays.asList(new Property("WLAN"))));
+			ArrayList<Property> bibWorkplaces = new ArrayList<Property>(Arrays.asList(new Property("Steckdose"),
+					new Property("LAN")));
+			Room roomBib1 = dataHelper.createPersistedRoom("Lesesaal 1", "Altbau", 0, bibWorkplaces);
+			Room roomBib2 = dataHelper.createPersistedRoom("Lesesaal 2", "Neubau", 1, bibWorkplaces);
+			Room roomBib3 = dataHelper.createPersistedRoom("Gruppenraum 1", "Altbau", 1, bibWorkplaces);
+			Room roomBib4 = dataHelper.createPersistedRoom("Gruppenraum 2", "Altbau", 1, bibWorkplaces);
+			Room roomBib5 = dataHelper.createPersistedRoom("Gruppenraum 3", "Altbau", 1, bibWorkplaces);
+			Room roomBib6 = dataHelper.createPersistedRoom("Gruppenraum 4", "Altbau", 1, bibWorkplaces);
+			Room roomBib7 = dataHelper.createPersistedRoom("Gruppenraum 5", "Altbau", 1, bibWorkplaces);
+			Collection<Room> rooms30502 = new ArrayList<Room>(Arrays.asList(roomBib3, roomBib4, roomBib5,
+					roomBib6, roomBib7));
+			// 03 workplaces
+			for (int j = 1; j < 13; j++) {
+				String name = "1." + j;
+				roomBib1.addContainedFacility(dataHelper.createPersistedWorkplace(name, emptyList));
+			}
+			building3.addContainedFacility(roomBib1);
+			for (int i = 1; i < 9; i++) {
+				for (int j = 1; j < 9; j++) {
+					String name = i + "." + j;
+					roomBib2.addContainedFacility(dataHelper.createPersistedWorkplace(name, emptyList));
+				}
+			}
+			building3.addContainedFacility(roomBib2);
+			for (Room tmp : rooms30502) {
+				for (int i = 1; i < 3; i++) {
+					for (int j = 1; j < 7; j++) {
+						String name = i + "." + j;
+						tmp.addContainedFacility(dataHelper.createPersistedWorkplace(name, atisWorkplaces));
+					}
+				}
+				building3.addContainedFacility(tmp);
+			}
+			facilitiesInseted = true;
 		}
 	}
 
