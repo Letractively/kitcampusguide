5aad69b0307113728be7da5c703208e404bb2d9c
uadgn <uadgn@student.kit.edu>
2012-02-24 13:42:04 +0000

    Facility Test 91% LineCoverage User Test 100% Line Coverage Workplace 100% Line Coverage

---
+++ arbeitsplatzsuche/src/test/java/edu/kit/pse/ass/entity/WorkplaceTest.java
@@ -0,0 +1,71 @@
+/**
+ * This tests the workplace entity
+ */
+package edu.kit.pse.ass.entity;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Sebastian
+ *
+ */
+public class WorkplaceTest {
+
+	/** The user. */
+	Workplace workplace;
+
+	/**
+	 * Sets the up.
+	 * 
+	 * @throws Exception
+	 *             the exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		workplace = new Workplace();
+	}
+	
+	/**
+	 * Test set parent facility.
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void testSetParentFacility() {
+		workplace.setParentFacility(null);
+	}
+	
+	/**
+	 * Test set parent facility.
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void testSetParentFacility2() {
+		workplace.setParentFacility(new Workplace());
+	}
+	
+	/**
+	 * Test set parent facility.
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void testSetParentFacility3() {
+		workplace.setParentFacility(new Building());
+	}
+	
+	/**
+	 * Test set parent facility.
+	 */
+	@Test
+	public void testSetParentFacility4() {
+		workplace.setParentFacility(new Room());
+		assertTrue(workplace.getParentFacility() != null);
+	}
+	
+	/**
+	 * Test add contained facility
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void testAddContainedFacility() {
+		workplace.addContainedFacility(null);
+	}
+}
