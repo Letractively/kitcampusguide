b433dd5142868afc4d17b825c62f406043722aad
philipp.schleier@student.kit.edu <philipp.schleier@student.kit.edu@02b7392b-9286-25d4-34bb-9ed0ff935e76>
2012-12-17 20:21:00 +0000

    Implementation idea for permission evaluator

--- branches/BA_Schleier/poimanager/poimanager-security/src/main/java/edu/kit/cm/kitcampusguide/access/evaluate/RoleOfMemberInGroupEvaluator.java
+++ branches/BA_Schleier/poimanager/poimanager-security/src/main/java/edu/kit/cm/kitcampusguide/access/evaluate/RoleOfMemberInGroupEvaluator.java
@@ -1,62 +1,94 @@
 package edu.kit.cm.kitcampusguide.access.evaluate;
 
-import java.io.Serializable;
+import java.io.InputStream;
 
-import org.springframework.security.access.PermissionEvaluator;
+import org.apache.log4j.Logger;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 
+import com.axiomatics.sdk.connections.PDPConnection;
+import com.axiomatics.sdk.connections.PDPConnectionFactory;
+import com.axiomatics.sdk.context.SDKResponseWithTrace;
+import com.axiomatics.sdk.context.XacmlRequestBuilder;
+import com.axiomatics.xacml.Constants;
+import com.axiomatics.xacml.reqresp.Request;
+
+import edu.kit.cm.kitcampusguide.model.MemberToGroupMapping;
 import edu.kit.cm.kitcampusguide.service.user.MemberUserDetails;
-import edu.kit.tm.cm.kitcampusguide.poiservice.Poi;
+import edu.kit.tm.cm.kitcampusguide.poiservice.CreateRequestComplexType;
+import edu.kit.tm.cm.kitcampusguide.poiservice.DeleteRequestComplexType;
+import edu.kit.tm.cm.kitcampusguide.poiservice.ReadRequestComplexType;
+import edu.kit.tm.cm.kitcampusguide.poiservice.UpdateRequestComplexType;
 
-public class RoleOfMemberInGroupEvaluator implements PermissionEvaluator {
+public class RoleOfMemberInGroupEvaluator {
 
-    @Override
-    public boolean hasPermission(Authentication authentication, Object targetDomainObject, Object permission) {
-        return true;
-//    	boolean hasPermission = false;
-//        if (authentication instanceof MemberUserDetails) {
-//            MemberUserDetails user = (MemberUserDetails) authentication;
-//            hasPermission = hasUserReadPermissionFor(targetDomainObject, user);
-//        }
-//        return hasPermission;
+    private static final Logger log = Logger.getLogger(RoleOfMemberInGroupEvaluator.class);
+    
+    public boolean hasPermission(Object targetDomainObject) {
+    	boolean decision = false;
+    	SecurityContext authentication = SecurityContextHolder.getContext();
+    	/*if(authentication.isAuthenticated())
+    		log.info("ABC AUTHOK");
+    	else
+    		log.info("ABC AUTHNOKE");
+    	
+    	MemberUserDetails user = (MemberUserDetails) authentication.getPrincipal();
+    	
+    	log.info("XACML: Build request for: " + targetDomainObject.getClass().getName());
+    	if(targetDomainObject instanceof UpdateRequestComplexType) {
+    		//PoiWithId poi = ((UpdateRequestComplexType) targetDomainObject).getPoi();
+    		decision = pdpRequest(user, "update");
+    	} else if(targetDomainObject instanceof CreateRequestComplexType) {
+    		// Returns Poi instead of PoiWithId :-(
+    		decision = pdpRequest(user, "create");
+    	} else if(targetDomainObject instanceof DeleteRequestComplexType) {
+    		// Doesn't have any method to get poi. Only knows the ID (need the groups)
+    		decision = pdpRequest(user, "delete");
+    	} else if(targetDomainObject instanceof ReadRequestComplexType) {
+    		decision = pdpRequest(user, "read");
+    	}*/
+    	return decision;
     }
+    
+    private boolean pdpRequest(MemberUserDetails user, String action) {
+    	SDKResponseWithTrace resp = null;
+    	try {
+			InputStream props = Thread.currentThread().getContextClassLoader().getResourceAsStream("pdp.properties");
+			
+    		PDPConnection pdpConnection = PDPConnectionFactory.getPDPConnection(props);
+			XacmlRequestBuilder reqBuild = new XacmlRequestBuilder();
+    		
+			log.info("XACML: Adding action-id \"" + action + "\"");
+			reqBuild.addActionAttribute(Constants.ACTION_ID, action);
+			
+    		for(MemberToGroupMapping mapping : user.getGroupMappings()) {
+    			String role = mapping.getRole();
+    			String group = mapping.getGroup().getName();
+    			log.info("XACML: Adding group \"" + group + "\" to role " + role);
+    			//reqBuild.addSubjectAttribute("urn:kit:roles:"+role, group);
+    			reqBuild.addSubjectAttribute("urn:kit:roles:"+role, "hasRole");
+    		}
+    		
+    		/*Strings groupIds = poi.getGroupIds();
+    		for(String groupId : groupIds.getId()) {
+    			System.out.println("adding resource group: " + groupId);
+    			reqBuild.addResourceAttribute(Constants.RESOURCE_ID, groupId);
+    		}*/
 
-    private boolean hasUserReadPermissionFor(Object targetDomainObject, MemberUserDetails user) {
-        boolean hasPermission = false;
-        if (targetDomainObject instanceof Poi) {
-            Poi poi = (Poi) targetDomainObject;
-            hasPermission = isPoiPublicOrHasUserReadPermission(poi, user);
-        }
-        return hasPermission;
-    }
+			Request req = reqBuild.buildRequest();
+							
+			resp = pdpConnection.evaluateWithTrace(req);
 
-    private boolean isPoiPublicOrHasUserReadPermission(Poi poi, MemberUserDetails user) {
-        boolean hasPermission;
-        if (poi.isPublicly()) {
-            hasPermission = true;
-        } else {
-            hasPermission = hasUserReadPermissionForPoi(user, poi);
-        }
-        return hasPermission;
-    }
+			log.info("XACML: Answer = "+resp.getResponse().toString());
+			
+	    	return resp.getBiasedDecision();
 
-    private boolean hasUserReadPermissionForPoi(MemberUserDetails user, Poi poi) {
-        boolean hasPermission = false;
-
-        for (String groupId : poi.getGroupIds().getId()) {
-            if (user.hasAdminRoleForGroupWithId(groupId) || user.hasUserRoleForGroupWithId(groupId)) {
-
-                hasPermission = true;
-            }
-        }
-        return hasPermission;
-    }
-
-    @Override
-    public boolean hasPermission(Authentication authentication, Serializable targetId, String targetType,
-            Object permission) {
-        // Object should be loaded before authentication
-        return false;
+    	} catch(Exception e) {
+			e.printStackTrace();
+		}
+    	
+    	return false;
     }
 
 }
